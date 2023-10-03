dcc9d0d4bb1e7ee6580afb2ea0611777aa00a8aa
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-02 22:44:41 -0500

    Update util-db.php

--- util-db.php
+++ util-db.php
@@ -2,7 +2,7 @@
 function get_db_connection(){
     // Create connection
     // You can use Local Host
-    $conn = new mysqli('159.89.47.44', 'fpgarcia_hw3user', 'Kamx4068', 'fpgarcia_HW3-MIS4013');
+    $conn = new mysqli('159.89.47.44', 'fpgarcia_hw3user', 'Sd.PA,N]p*0O', 'fpgarcia_HW3-MIS4013');
     
     // Check connection
     if ($conn->connect_error) {
