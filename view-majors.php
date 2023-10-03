1e3ed9edacb31b3563e6291eee06a90d90f6492b
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-02 23:31:32 -0500

    Update view-majors.php

--- view-majors.php
+++ view-majors.php
@@ -1,1 +1,26 @@
-
+<h1>Majors</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>Program Code</th>
+      <th>Major</th>
+      <th>Division</th>
+      <th>Minimum Credit Hours</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php 
+      while ($major = $majors->fetch_assoc()) {
+      ?>
+      <tr>
+        <td><?php echo $major['program_code']; ?></td>
+        <td><?php echo $major['major_name']; ?></td>
+        <td><?php echo $major['division']; ?></td>
+        <td><?php echo $major['minimum_credit_hours']; ?></td>
+      </tr>
+      }
+      ?>
+    </tbody>
+  </table>
+</div>
