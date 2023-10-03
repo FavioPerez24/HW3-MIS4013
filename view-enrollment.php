b13075393dcf950d9d4241aa47e74625269e7e20
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-03 13:53:39 -0500

    Update view-enrollment.php

--- view-enrollment.php
+++ view-enrollment.php
@@ -1,1 +1,28 @@
-
+<h1>Majors</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>Program Code</th>
+      <th>Major</th>
+      <th>Division</th>
+      <th>Minimum Credit Hours</th>
+      <th>1</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php 
+      while ($major = $majors->fetch_assoc()) {
+      ?>
+      <tr>
+        <td><?php echo $major['program_code']; ?></td>
+        <td><?php echo $major['major_name']; ?></td>
+        <td><?php echo $major['division']; ?></td>
+        <td><?php echo $major['minimum_credit_hours']; ?></td>
+        <td><a href="potential-industry-by-major.php?code=<?php echo $major['program_code']; ?>">Industry</a></td>
+      </tr>
+      }
+      ?>
+    </tbody>
+  </table>
+</div>
