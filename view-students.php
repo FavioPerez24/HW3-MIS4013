164471fe5f0512e7e9e169cc64c176e9979e0435
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-02 22:27:10 -0500

    Update view-students.php

--- view-students.php
+++ view-students.php
@@ -1,19 +1,23 @@
-
+<h1>Students</h1>
 <div class="table-responsive">
   <table class="table">
     <thead>
+      <tr>
       <th>ID</th>
-      <th>Name</th>
-      <th>Office</th>
+      <th>First Name</th>
+      <th>Last Name</th>
+      <th>Graduation Year</th>
+      </tr>
     </thead>
     <tbody>
       <?php 
-      while ($instructor = $instructors->fetch_assoc()) {
+      while ($student = $students->fetch_assoc()) {
       ?>
       <tr>
-        <td><?php echo $instructor['instructor_id']; ?></td>
-        <td><?php echo $instructor['instructor_name']; ?></td>
-        <td><?php echo $instructor['instructor_office_name']; ?></td>
+        <td><?php echo $student['Student_ID']; ?></td>
+        <td><?php echo $student['Student_FirstName']; ?></td>
+        <td><?php echo $student['Student_LastName']; ?></td>
+        <td><?php echo $student['Graduation_Year']; ?></td>
       </tr>
       }
       ?>
