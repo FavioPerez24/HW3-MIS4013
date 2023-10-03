74c96177c80a611da3e9dcbf543de3d373280b88
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-02 23:11:34 -0500

    Update majors.php

--- majors.php
+++ majors.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-majors.php");
 
+$pageTitle= "Majors";
+Include "view-header.php";
+$majors = selectMajors();
+Include "view-students.php";
+Include "view-footer.php";
+?>
