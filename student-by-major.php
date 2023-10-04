37d543028403a576f3503620db0f798900d63523
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-03 23:12:07 -0500

    Update and rename enrollment.php to student-by-major.php

---
+++ student-by-major.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-student-by-major.php");
+
+$pageTitle= "Student by Major";
+Include "view-header.php";
+$majors = selectStudentbyMajor($_GET['id']);
+Include "view-studentbymajor.php";
+Include "view-footer.php";
+?>
