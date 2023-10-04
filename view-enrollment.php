c65f25ad4753ee9f3dedfefde3f4d1c283556ccf
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-04 12:01:49 -0500

    Update view-enrollment.php

--- view-enrollment.php
+++ view-enrollment.php
@@ -9,14 +9,21 @@
     <div class="card-body">
       <h5 class="card-title"><td><?php echo $student['Student_FirstName']; ?></td><td> <?php echo $student['Student_LastName']; ?></td></h5>
       <p class="card-text"> 
+      
+      <ul class="list-group">
       <?php
         $majors= selectStudentbyMajor($student['Student_ID']);
         while ($major = $majors->fetch_assoc()) {
       ?>
-//list courses  
+      <li class="list-group-item"><td><?php echo $major['program_code']; ?></td></li>
+      <li class="list-group-item"><td><?php echo $major['major_name']; ?></td></li>
+      <li class="list-group-item"><td><?php echo $major['minimum_credit_hours']; ?></td></li>
+      <li class="list-group-item"><td><?php echo $major['division']; ?></td></li>
+
       <?php
         }
       ?>
+      </ul>
       </p>
       <p class="card-text"><small class="text-body-secondary"><td><?php echo $student['Graduation_Year']; ?></td></small></p>
     </div>
