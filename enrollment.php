2bc97de0367675cd3f28193506822b72af53beb2
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-04 11:19:52 -0500

    Update enrollment.php

--- enrollment.php
+++ enrollment.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-enrollment.php");
 
+$pageTitle= "Enrollment";
+Include "view-header.php";
+$students = selectStudents();
+Include "view-enrollment.php";
+Include "view-footer.php";
+?>
