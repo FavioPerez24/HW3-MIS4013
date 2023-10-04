f3cf972b5e1d72cad8c48dde7c3cadc58cbdc09a
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-04 12:05:02 -0500

    Update view-enrollment.php

--- view-enrollment.php
+++ view-enrollment.php
@@ -15,11 +15,10 @@
         $majors= selectStudentbyMajor($student['Student_ID']);
         while ($major = $majors->fetch_assoc()) {
       ?>
-      <li class="list-group-item"><td><?php echo $major['program_code']; ?></td></li>
+      <li class="list-group-item">Code:</li> ><td><?php echo $major['program_code']; ?></td></li>
+      <li class="list-group-item"></li>
       <li class="list-group-item"><td><?php echo $major['major_name']; ?></td></li>
       <li class="list-group-item"><td><?php echo $major['minimum_credit_hours']; ?></td></li>
-      <li class="list-group-item"><td><?php echo $major['division']; ?></td></li>
-
       <?php
         }
       ?>
