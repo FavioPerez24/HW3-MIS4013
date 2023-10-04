a03b43d73872dc18f7ee92a2012ef3098ef47a3f
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-03 22:34:55 -0500

    Update enrollment.php

--- enrollment.php
+++ enrollment.php
@@ -4,7 +4,7 @@ require_once("model-enrollment.php");
 
 $pageTitle= "Enrollment";
 Include "view-header.php";
-$majors = selectEnrollment($_GET['E.program_code']);
+$majors = selectEnrollment($_GET['program_code']);
 Include "view-enrollment.php";
 Include "view-footer.php";
 ?>
