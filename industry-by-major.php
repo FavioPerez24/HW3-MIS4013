9ccbf55b3c118bca6fe52c2882bb2c50f2ccf880
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-04 10:03:52 -0500

    Create industry-by-major.php

---
+++ industry-by-major.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-industry-by-major.php");
+
+$pageTitle= "Industry by Major";
+Include "view-header.php";
+$majors = selectIndustrybyMajor($_POST['mid']);
+Include "view-industry-by-major.php";
+Include "view-footer.php";
+?>
