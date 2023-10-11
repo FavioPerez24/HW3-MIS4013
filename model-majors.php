bcc9b7935d9cecd24a1b65335ed7684f71828586
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-11 10:49:24 -0500

    Update model-majors.php

--- model-majors.php
+++ model-majors.php
@@ -12,4 +12,46 @@ function selectMajors() {
         throw $e;
     }
 }
+
+function inserttMajors($mName, $mDivision) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `Business_Major` (`major_name`, `division`) VALUES (?,?)");
+        $stmt->bind_param("ss", $mName, $mDivsion);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updatetMajors($mName, $mDivision, $mProgramCode) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `Business_Major` set `major_name`= ?, `division`= ? where program_code = ?");
+        $stmt->bind_param("ssi", $mName, $mDivsion, $mProgramCode);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deletetMajors($mProgramCode) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM Business_Major where program_code = ?");
+        $stmt->bind_param("i", $mProgramCode);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
