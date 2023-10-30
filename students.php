c32914691369c8bd2a0603fc8c34ef9b273cccfb
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-30 17:12:40 -0500

    Update students.php

--- students.php
+++ students.php
@@ -8,21 +8,21 @@ Include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertStudent($_POST['sFName'], $_POST['sLName'], $_POST['sGrad'], $_POST['aID'])) {
+      if (insertStudent($_POST['sFName'], $_POST['sLName'], $_POST['sGrad'], $_POST['aid'])) {
         echo '<div class="alert alert-success" role="alert">Student added.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
     case "Edit":
-      if (updateStudent($_POST['sFName'], $_POST['sLName'], $_POST['sGrad'], $_POST['sID'], $_POST['aID'])) {
+      if (updateStudent($_POST['sFName'], $_POST['sLName'], $_POST['sGrad'], $_POST['sid'], $_POST['aid'])) {
         echo '<div class="alert alert-success" role="alert">Student edited.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
     case "Delete":
-      if (deleteStudent($_POST['sID'])) {
+      if (deleteStudent($_POST['sid'])) {
         echo '<div class="alert alert-success" role="alert">Student deleted.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
