4a527eaedba69d8e7c57618536cd15b2e69b0fd4
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-30 11:15:52 -0500

    Update students.php

--- students.php
+++ students.php
@@ -4,6 +4,33 @@ require_once("model-students.php");
 
 $pageTitle= "Students";
 Include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertStudent($_POST['sFName'], $_POST['sLName'], $_POST['sGrad'], $_POST['aID'])) {
+        echo '<div class="alert alert-success" role="alert">Student added.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Edit":
+      if (updateStudent($_POST['sFName'], $_POST['sLName'], $_POST['sGrad'],)) {
+        echo '<div class="alert alert-success" role="alert">Student edited.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Delete":
+      if (deleteStudent($_POST['sID'])) {
+        echo '<div class="alert alert-success" role="alert">Student deleted.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+  }
+}
+
 $students = selectStudents();
 Include "view-students.php";
 Include "view-footer.php";
