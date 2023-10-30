ca8681583bfc3ba4d972b357b1f85edf0964515c
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-30 12:05:40 -0500

    Update model-students.php

--- model-students.php
+++ model-students.php
@@ -12,4 +12,46 @@ function selectStudents() {
         throw $e;
     }
 }
+
+function insertStudent($sFName, $sLName, $sGrad) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `Student` (`Student_FirstName`, `Student_LastName`, `Graduation_Year`) VALUES (?, ?, ?)");
+        $stmt->bind_param("ssi", $sFName, $sLName, $sGrad);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateStudent($sFName, $sLName, $sGrad, $sID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `Student` set `Student_FirstName` = ?, `Student_LastName` = ?,  `Graduation_Year` = ? where Student_ID = ?");
+        $stmt->bind_param("ssii", $sFName, $sLName, $sGrad, $sID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteStudent($sID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from Student where Student_ID= ?");
+        $stmt->bind_param("i", $sID);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
