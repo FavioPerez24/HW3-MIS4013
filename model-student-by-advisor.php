f478b99030e3a240c8d889f45a1a6d18b837e590
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-30 10:31:45 -0500

    Update model-student-by-advisor.php

--- model-student-by-advisor.php
+++ model-student-by-advisor.php
@@ -2,7 +2,7 @@
 function selectStudentbyAdvisor($aid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT Advisor_Name, Meeting_Times FROM `Advisor`A JOIN Student S ON A.Advisor_ID=S.Advisor_ID WHERE A.Advisor_ID=?;");
+        $stmt = $conn->prepare("SELECT Advisor_Name, Meeting_Times FROM `Advisor`A JOIN Student S ON A.Advisor_ID=S.Advisor_ID WHERE S.Student_ID=?;");
         $stmt->bind_param("i", $aid);
         $stmt->execute();
         $result = $stmt->get_result();
