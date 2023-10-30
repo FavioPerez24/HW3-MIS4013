f376d0018804e6f6f3baa5b5983458b207dae133
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-30 10:06:09 -0500

    Update view-industry-by-major.php

--- view-industry-by-major.php
+++ view-industry-by-major.php
@@ -1,25 +1,21 @@
-<h1>Industry by Major</h1>
+<h1>Student's Advisor</h1>
 <div class="table-responsive">
   <table class="table">
     <thead>
       <tr>
-      <th>Program Code</th>
-      <th>Major</th>
-      <th>Division</th>
-      <th>Minimum Credit Hours</th>
-      <th>Potential Industry</th>
+      <th>Advisor ID</th>
+      <th>Advisor Name</th>
+      <th>Meeting Times</th>
       </tr>
     </thead>
     <tbody>
       <?php 
-      while ($industry = $industries->fetch_assoc()) {
+      while ($advisor = $advisors->fetch_assoc()) {
       ?>
       <tr>
-        <td><?php echo $industry['program_code']; ?></td>
-        <td><?php echo $industry['major_name']; ?></td>
-        <td><?php echo $industry['division']; ?></td>
-        <td><?php echo $industry['minimum_credit_hours']; ?></td>
-        <td><?php echo $industry['Industry_Name']; ?></td>
+        <td><?php echo $advisor['Advisor_ID']; ?></td>
+        <td><?php echo $advisor['Advisor_Name']; ?></td>
+        <td><?php echo $advisor['Meeting_Times']; ?></td>
       </tr>
       <?php
       }
