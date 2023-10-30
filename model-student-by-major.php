d3ee448208b9af1ff23afcbb015521aabc9f5dd4
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-30 17:16:31 -0500

    Update model-student-by-major.php

--- model-student-by-major.php
+++ model-student-by-major.php
@@ -32,7 +32,7 @@ function updateChoice($mName, $mDiv, $mCred, $mid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("update `Business_Major` set `major_name` = ?, `division` = ?,  `minimum_credit_hours` = ? where program_code = ?");
-        $stmt->bind_param("ssii", $mName, $mDiv, $mCred, $mID);
+        $stmt->bind_param("ssii", $mName, $mDiv, $mCred, $mid);
         $success = $stmt->execute();
         $conn->close();
         return $success;
@@ -46,7 +46,7 @@ function deleteChoice($mid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("delete from Business_Major where program_code= ?");
-        $stmt->bind_param("i", $mID);
+        $stmt->bind_param("i", $mid);
         $success = $stmt->execute();
         $conn->close();
         return $success;
