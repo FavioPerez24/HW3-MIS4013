dc69acfc9d67a92f4811390a97d70f6cd1df49e3
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-30 12:25:56 -0500

    Update view-students.php

--- view-students.php
+++ view-students.php
@@ -1,4 +1,13 @@
+<div class="row">
+  <div class="col">
 <h1>Students</h1>
+  </div>
+  <div class="col-auto">
+<?php
+include "view-students-newform.php";
+?>
+  </div>
+</div>
 <div class="table-responsive">
   <table class="table">
     <thead>
@@ -7,8 +16,10 @@
       <th>First Name</th>
       <th>Last Name</th>
       <th>Graduation Year</th>
-      <th>Major</th>
-      <th>Advisor</th>
+      <th></th>
+      <th></th>
+        <th></th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
@@ -20,13 +31,26 @@
         <td><?php echo $student['Student_FirstName']; ?></td>
         <td><?php echo $student['Student_LastName']; ?></td>
         <td><?php echo $student['Graduation_Year']; ?></td>
+        <td><?php include "view-student-editform.php"; ?></td>
+        <td>
+      <form method="post" action="">
+        <input type="hidden" name="sid" value="<?php echo $student['Student_ID']; ?>">
+        <input type="hidden" name="actionType" value="Delete">
+        <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?');">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+          </svg>
+        </button>
+      </form>
+    </td>
         <td><a href="student-by-major.php?id=<?php echo $student['Student_ID']; ?>">Major</a></td>
         <td>
           <form method="post" action="student-by-advisor.php">
           <input type="hidden" name="aid" value="<?php echo $student['Student_ID']; ?>"> 
           <button type="submit" class="btn btn-primary">Advisor</button>
           </form>
-        </td>      
+        </td>  
       </tr>
       <?php
       }
