7f8803d1c58d8b52f25299b70f8560291d99a174
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-30 11:11:12 -0500

    Update majors.php

--- majors.php
+++ majors.php
@@ -4,6 +4,33 @@ require_once("model-majors.php");
 
 $pageTitle= "Majors";
 Include "view-header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertMajor($_POST['mName'], $_POST['mDiv'], $_POST['mCred'])) {
+        echo '<div class="alert alert-success" role="alert">Major added.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Edit":
+      if (updateMajor($_POST['mName'], $_POST['mDiv'], $_POST['mCred'], $_POST['mID'])) {
+        echo '<div class="alert alert-success" role="alert">Major edited.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Delete":
+      if (deleteMajor($_POST['mID'])) {
+        echo '<div class="alert alert-success" role="alert">Major deleted.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+  }
+}
+
 $majors = selectMajors();
 Include "view-majors.php";
 Include "view-footer.php";
