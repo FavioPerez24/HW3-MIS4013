92eb39f497bfef476bc5e57fce06330401625906
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-30 10:00:37 -0500

    Update view-majors.php

--- view-majors.php
+++ view-majors.php
@@ -19,7 +19,6 @@
         <td><?php echo $major['major_name']; ?></td>
         <td><?php echo $major['division']; ?></td>
         <td><?php echo $major['minimum_credit_hours']; ?></td>
-        <td><a href="student-by-major.php?id=<?php echo $student['Student_ID']; ?>">Student</a></td>
       </tr>
       <?php
       }
