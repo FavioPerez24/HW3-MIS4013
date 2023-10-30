2e991b3fa368c2dcb7db9d446b40388e94ba7753
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-30 13:27:47 -0500

    Update view-enrollment.php

--- view-enrollment.php
+++ view-enrollment.php
@@ -18,6 +18,9 @@
       <li class="list-group-item">Code: <td><?php echo $major['program_code']; ?></td></li>
       <li class="list-group-item">Major: <td><?php echo $major['major_name']; ?></td></li>
       <li class="list-group-item">Minimum credits: <td><?php echo $major['minimum_credit_hours']; ?></td></li>
+      <li class="list-group-item"><td><?php include $major['minimum_credit_hours']; ?></td></li>
+      <li class="list-group-item"><td><?php include $major['minimum_credit_hours']; ?></td></li>
+
       <?php
         }
       ?>
