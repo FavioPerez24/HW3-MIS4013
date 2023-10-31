12e0add969645f058b7d08904889aab3222ea9a1
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-30 23:02:50 -0500

    Update student-by-major.php

--- student-by-major.php
+++ student-by-major.php
@@ -8,21 +8,21 @@ Include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertChoice($_POST['mName'], $_POST['mDiv'], $_POST['mCred'])) {
+      if (insertChoice($_POST['mName2'], $_POST['mDiv2'], $_POST['mCred2'])) {
         echo '<div class="alert alert-success" role="alert"> New Major added.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
     case "Edit":
-      if (updateChoice($_POST['mName'], $_POST['mDiv'], $_POST['mCred'], $_POST['mid'])) {
+      if (updateChoice($_POST['mName2'], $_POST['mDiv2'], $_POST['mCred2'], $_POST['mid2'])) {
         echo '<div class="alert alert-success" role="alert">Major edited.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
     case "Delete":
-      if (deleteChoice($_POST['mid'])) {
+      if (deleteChoice($_POST['mid2'])) {
         echo '<div class="alert alert-success" role="alert">Major deleted.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
