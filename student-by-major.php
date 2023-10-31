df84681bcac6e454bcf076dd687f692e4bbe8da1
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-31 16:53:03 -0500

    Update student-by-major.php

--- student-by-major.php
+++ student-by-major.php
@@ -31,7 +31,7 @@ if (isset($_POST['actionType'])) {
   }
 }
 
-$majors = selectStudentbyMajor($_POST['id']);
+$majors = selectStudentbyMajor($_GET['id']);
 Include "view-student-by-major.php";
 Include "view-footer.php";
 ?>
