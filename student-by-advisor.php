06fd63bb2505c54922c8e603abd7bf87dd349044
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-30 22:56:22 -0500

    Update student-by-advisor.php

--- student-by-advisor.php
+++ student-by-advisor.php
@@ -4,7 +4,7 @@ require_once("model-student-by-advisor.php");
 
 $pageTitle= "Student's Advisor";
 Include "view-header.php";
-$advisors = selectStudentbyAdvisor($_POST['aid']);
+$advisors = selectStudentbyAdvisor($_POST['aID']);
 Include "view-student-by-advisor.php";
 Include "view-footer.php";
 ?>
