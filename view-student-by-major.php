bafc27cd91f125a24d0e111df0da7f464599ae5f
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-30 23:05:12 -0500

    Update view-student-by-major.php

--- view-student-by-major.php
+++ view-student-by-major.php
@@ -32,7 +32,7 @@ include "view-studentbymajor-newform.php";
         <td><?php include "view-studentbymajor-editform.php"; ?></td>
         <td>
       <form method="post" action="">
-        <input type="hidden" name="mid2" value="<?php echo $major['program_code']; ?>">
+        <input type="hidden" name="mid" value="<?php echo $major['program_code']; ?>">
         <input type="hidden" name="actionType" value="Delete">
         <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure?');">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
