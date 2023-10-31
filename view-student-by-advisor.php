e0e0b17f3b244bea301e5a2f92c8a06214943d9e
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-31 18:43:46 -0500

    Update view-student-by-advisor.php

--- view-student-by-advisor.php
+++ view-student-by-advisor.php
@@ -1,4 +1,13 @@
+<div class="row">
+  <div class="col">
 <h1>Student's Advisor</h1>
+  </div>
+  <div class="col-auto">
+<?php
+include "view-studentbyadvisor-newform.php";
+?>
+  </div>
+</div>
 <div class="table-responsive">
   <table class="table">
     <thead>
@@ -6,6 +15,8 @@
       <th>Advisor ID</th>
       <th>Advisor Name</th>
       <th>Meeting Times</th>
+      <th></th>
+      <th></th>  
       </tr>
     </thead>
     <tbody>
@@ -16,6 +27,8 @@
         <td><?php echo $advisor['Advisor_ID']; ?></td>
         <td><?php echo $advisor['Advisor_Name']; ?></td>
         <td><?php echo $advisor['Meeting_Times']; ?></td>
+        <td></td>
+        <td></td>
       </tr>
       <?php
       }
