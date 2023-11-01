fdc2ce9a0618fd4f9a40822219fd0a89da7b897a
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-31 21:31:58 -0500

    Update major-input-list.php

--- major-input-list.php
+++ major-input-list.php
@@ -1,1 +1,13 @@
-
+<select class="form-select" id="mid" name="mid">
+<?php
+while ($majorItem = $majorList->fetch_assoc()) {
+  $selText = "";
+  if ($selectedMajor == $majorItem['program_code']) {
+    $selText = " selected";
+  }
+?>
+  <option value="<?php echo $majorItem['program_code']; ?>"<?=$selText?>><?php echo $majorItem['major_name']; ?></option>
+<?php
+}
+?>
+</select>
