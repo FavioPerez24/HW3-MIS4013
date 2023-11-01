fe8a62e5eb5ba569ee0d25b4b11bce17be8cfd76
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-31 20:10:32 -0500

    Update model-student-by-advisor.php

--- model-student-by-advisor.php
+++ model-student-by-advisor.php
@@ -13,4 +13,46 @@ function selectStudentbyAdvisor($aid) {
         throw $e;
     }
 }
+
+function insertAdvisor($aName, $aMT) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `Advisor` (`Advisor_Name`, `Meeting_Times`) VALUES (?, ?)");
+        $stmt->bind_param("ss", $aName, $aMT);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updateChoice($aName, $aMT, $aid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `Advisor` set `Advisor_Name` = ?, `Meeting_Times` = ? where Advisor_ID = ?");
+        $stmt->bind_param("ssi", $aName, $aMT, $aid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteChoice($aid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from Advisor where Advisor_ID= ?");
+        $stmt->bind_param("i", $aid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
