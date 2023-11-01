c2ca3996686adbdbbf094a6f228cfc0f6ec826f7
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-31 21:29:28 -0500

    Update student-input-list.php

--- student-input-list.php
+++ student-input-list.php
@@ -1,1 +1,13 @@
-
+<select class="form-select" id="sid" name="sid">
+<?php
+while ($studentItem = $studentList->fetch_assoc()) {
+  $selText = "";
+  if ($selectedStudent == $studentItem['Student_ID']) {
+    $selText = " selected";
+  }
+?>
+  <option value="<?php echo $studentItem['Student_ID']; ?>"<?=$selText?>><?php echo $studentItem['Student_LastName ', 'Student_FirstName'"]; ?></option>
+<?php
+}
+?>
+</select>
