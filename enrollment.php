e62958229e2c93688810db887720072a622d7600
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-31 21:12:33 -0500

    Update enrollment.php

--- enrollment.php
+++ enrollment.php
@@ -22,7 +22,7 @@ if (isset($_POST['actionType'])) {
       }
       break;
     case "Delete":
-      if (deleteEnrollment($_POST['mid'])) {
+      if (deleteEnrollment($_POST['eid'])) {
         echo '<div class="alert alert-success" role="alert">Enrollment deleted.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
