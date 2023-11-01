5e99bbf03347160b39634c140e0cb85c7b8d48d2
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-10-31 20:19:49 -0500

    Update view-studentbyadvisor-newform.php

--- view-studentbyadvisor-newform.php
+++ view-studentbyadvisor-newform.php
@@ -1,5 +1,5 @@
 <!-- Button trigger modal -->
-<button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#newChoiceModal">
+<button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#newAdvisorModal">
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
       <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
       <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
@@ -7,26 +7,22 @@
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="newChoiceModal" tabindex="-1" aria-labelledby="newChoiceModalLabel" aria-hidden="true">
+<div class="modal fade" id="newAdvisorModal" tabindex="-1" aria-labelledby="newAdvisorModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="newChoiceModalLabel">Add second major</h1>
+        <h1 class="modal-title fs-5" id="newAdvisorModalLabel">Add second advisor</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         <form method="post" action="">
           <div class="mb-3">
-            <label for="mName" class="form-label">Major Name</label>
+            <label for="aName" class="form-label">Advisor Name</label>
             <input type="text" class="form-control" id="mName" name="mName">
           </div>
           <div class="mb-3">
-            <label for="mDiv" class="form-label">Division</label>
-            <input type="text" class="form-control" id="mDiv" name="mDiv">
-          </div>
-          <div class="mb-3">
-            <label for="mCred" class="form-label">Minimum Credit Hours</label>
-            <input type="number" class="form-control" id="mCred" name="mCred">
+            <label for="aMT" class="form-label">Meeting Times</label>
+            <input type="text" class="form-control" id="aMT" name="aMT">
           </div>
             <input type="hidden" name="actionType" value="Add">
           <button type="submit" class="btn btn-primary">Save</button>
