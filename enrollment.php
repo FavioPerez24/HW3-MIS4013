c04a800053281b1978b3ee48f1db04fa00d1d887
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-11-05 16:32:28 -0600

    Update enrollment.php

--- enrollment.php
+++ enrollment.php
@@ -8,14 +8,14 @@ Include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertEnrollment($_POST['mid'], $_POST['sid'],  $_POST['GPA'])) {
+      if (insertEnrollment($_POST['mid'], $_POST['sid'])) {
         echo '<div class="alert alert-success" role="alert">Enrollment added.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
     case "Edit":
-      if (updateEnrollment($_POST['mid'], $_POST['sid'],  $_POST['GPA'], $_POST['eid'])) {
+      if (updateEnrollment($_POST['mid'], $_POST['sid'], $_POST['eid'])) {
         echo '<div class="alert alert-success" role="alert">Enrollment edited.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
