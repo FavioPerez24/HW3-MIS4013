fe6a1f0cb4b23719174d7ce4554634bf5827b63c
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-11-05 16:44:01 -0600

    Update students.php

--- students.php
+++ students.php
@@ -8,14 +8,14 @@ Include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertStudent($_POST['sFName'], $_POST['sLName'], $_POST['sGrad'], $_POST['aid'])) {
+      if (insertStudent($_POST['sFName'], $_POST['sGrad'], $_POST['aid'])) {
         echo '<div class="alert alert-success" role="alert">Student added.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
     case "Edit":
-      if (updateStudent($_POST['sFName'], $_POST['sLName'], $_POST['sGrad'], $_POST['aid'], $_POST['sid'])) {
+      if (updateStudent($_POST['sFName'], $_POST['sGrad'], $_POST['aid'], $_POST['sid'])) {
         echo '<div class="alert alert-success" role="alert">Student edited.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
