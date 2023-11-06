2aee4a9c05ec4e4a0c541b39274ec787c27bb643
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-11-06 11:30:14 -0600

    Update student-by-major.php

--- student-by-major.php
+++ student-by-major.php
@@ -4,33 +4,6 @@ require_once("model-student-by-major.php");
 
 $pageTitle= "Student by Major";
 Include "view-header.php";
-
-if (isset($_POST['actionType'])) {
-  switch ($_POST['actionType']) {
-    case "Add":
-      if (insertChoice($_POST['mName'], $_POST['mDiv'], $_POST['mCred'])) {
-        echo '<div class="alert alert-success" role="alert"> New Major added.</div>';
-      } else {
-        echo '<div class="alert alert-danger" role="alert">Error.</div>';
-      }
-      break;
-    case "Edit":
-      if (updateChoice($_POST['mName'], $_POST['mDiv'], $_POST['mCred'], $_POST['mid'])) {
-        echo '<div class="alert alert-success" role="alert">Major edited.</div>';
-      } else {
-        echo '<div class="alert alert-danger" role="alert">Error.</div>';
-      }
-      break;
-    case "Delete":
-      if (deleteChoice($_POST['mid'])) {
-        echo '<div class="alert alert-success" role="alert">Major deleted.</div>';
-      } else {
-        echo '<div class="alert alert-danger" role="alert">Error.</div>';
-      }
-      break;
-  }
-}
-
 $majors = selectStudentbyMajor($_GET['id']);
 Include "view-student-by-major.php";
 Include "view-footer.php";
