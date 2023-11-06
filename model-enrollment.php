6d4091304817dd2937f5505a6cf1297ae6386fde
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-11-06 13:20:57 -0600

    Update model-enrollment.php

--- model-enrollment.php
+++ model-enrollment.php
@@ -15,7 +15,7 @@ function selectStudents() {
 function selectStudentbyMajor($sid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT M.program_code, major_name, division, minimum_credit_hours, Enrollment_ID FROM `Business_Major`M JOIN Enrollment E ON E.program_code = M.program_code WHERE E.Student_ID=?");
+        $stmt = $conn->prepare("SELECT M.program_code, major_name, division, minimum_credit_hours, Enrollment_ID FROM `Business_Major` M JOIN Enrollment E ON E.program_code = M.program_code WHERE E.Student_ID=?");
         $stmt->bind_param("i", $sid);
         $stmt->execute();
         $result = $stmt->get_result();
