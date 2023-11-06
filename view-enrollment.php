78e108d825050f21b9bf3667cacc7433d8280429
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-11-06 10:58:46 -0600

    Update view-enrollment.php

--- view-enrollment.php
+++ view-enrollment.php
@@ -33,7 +33,7 @@ include "view-enrollment-editform.php";
       </div>
       <div class="col-auto">
         <form method="post" action="">
-          <input type="hidden" name="eid" value="<?php echo $major['program_code']; ?>">
+          <input type="hidden" name="eid" value="<?php echo $major['Enrollment_ID']; ?>">
           <input type="hidden" name="actionType" value="Delete">
           <button type="submit" class="btn" onclick="return confirm('Are you sure?');">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
