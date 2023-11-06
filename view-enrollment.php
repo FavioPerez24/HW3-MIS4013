c7f3095cdcfda8655eb6be6705a0504723d6b66c
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-11-06 11:20:42 -0600

    Update view-enrollment.php

--- view-enrollment.php
+++ view-enrollment.php
@@ -9,55 +9,36 @@
 
 <div class="card-group">
     <?php
-    $counter = 0;
+    $count = 0;
     while ($student = $students->fetch_assoc()) {
-        if ($counter % 2 == 0) {
-            // Starting a new row for every two cards
+        if ($count % 2 == 0) {
+            // Start a new row for every two cards
             echo '<div class="row">';
         }
     ?>
-    <div class="col">
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title"><?php echo $student['Student_FirstName']; ?></h5>
-                <p class="card-text">
-                    <ul class="list-group">
-                        <?php
-                        $majors = selectStudentByMajor($student['Student_ID']);
-                        while ($major = $majors->fetch_assoc()) {
-                        ?>
-                        <li class="list-group-item">
-                            <?php echo $major['major_name']; ?> - <?php echo $major['division']; ?> - <?php echo $major['minimum_credit_hours']; ?>
-                            <?php include "view-enrollment-editform.php"; ?>
-                            <form method="post" action="">
-                                <input type="hidden" name="eid" value="<?php echo $major['Enrollment_ID']; ?>">
-                                <input type="hidden" name="actionType" value="Delete">
-                                <button type="submit" class="btn" onclick="return confirm('Are you sure?');">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
-                                        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
-                                    </svg>
-                                </button>
-                            </form>
-                        </li>
-                        <?php } ?>
-                    </ul>
-                </p>
-                <p class="card-text"><small class="text-body-secondary">Graduation Year: <?php echo $student['Graduation_Year']; ?></small></p>
-            </div>
-        </div>
-    </div>
-    <?php
-        if ($counter % 2 != 0) {
-            // Closing the row after two cards
-            echo '</div>';
-        }
-        $counter++;
-    }
-    if ($counter % 2 != 0) {
-        // If the number of cards is odd, closing the last row
-        echo '</div>';
-    }
-    ?>
-</div>
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $student['Student_FirstName']; ?></h5>
+                    <p class="card-text">
+                        <ul class="list-group">
+                            <?php
+                            $majors = selectStudentByMajor($student['Student_ID']);
+                            while ($major = $majors->fetch_assoc()) {
+                            ?>
+                                <li class="list-group-item">
+                                    <div class="row">
+                                        <div class="col">
+                                            <?php echo $major['major_name']; ?> - <?php echo $major['division']; ?> - <?php echo $major['minimum_credit_hours']; ?>
+                                        </div>
+                                        <div class="col-auto">
+                                            <?php include "view-enrollment-editform.php"; ?>
+                                        </div>
+                                        <div class="col-auto">
+                                            <form method="post" action="">
+                                                <input type="hidden" name="eid" value="<?php echo $major['Enrollment_ID']; ?>">
+                                                <input type="hidden" name="actionType" value="Delete">
+                                                <button type="submit" class="btn" onclick="return confirm('Are you sure?');">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0
 
