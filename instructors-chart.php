bb8ed3640dad12f06f6e6b6f2dce838da6f6b6fd
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-11-13 12:37:22 -0600

    Create instructors-chart.php

---
+++ instructors-chart.php
@@ -0,0 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-students.php");
+
+$pageTitle= "Students Chart";
+Include "view/header.php";
+$students = selectStudents();
+Include "view/students-chart/page.php";
+Include "view/footer.php";
+?>
