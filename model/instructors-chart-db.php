b6d6b8a80003a976f5d740f5a0a1f7e381832d1f
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-11-13 12:47:08 -0600

    Update instructors-chart-db.php

--- model/instructors-chart-db.php
+++ model/instructors-chart-db.php
@@ -1,3 +1,15 @@
 <?php
-
+function selectStudents() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT Student_FirstName, count(E.Enrollment_ID) as num_enrollments FROM `Student` S join `Enrollment` E on E.Student_ID=S.Student_ID group by Student_FirstName");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
