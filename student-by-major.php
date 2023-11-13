227dbc501c7482315522ee8a1edabbb2a1e69081
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-11-13 13:43:08 -0600

    Update student-by-major.php

--- student-by-major.php
+++ student-by-major.php
@@ -1,10 +1,10 @@
 <?php
-require_once("util-db.php");
-require_once("model-student-by-major.php");
+require_once("connection.php");
+require_once("model/students-by-major.php");
 
 $pageTitle= "Student by Major";
 Include "view/header.php";
 $majors = selectStudentbyMajor($_GET['id']);
-Include "view-student-by-major.php";
+Include "view/student-by-major.php";
 Include "view/footer.php";
 ?>
