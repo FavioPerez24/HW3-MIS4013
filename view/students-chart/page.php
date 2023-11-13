9d3ed1e0dea463cb8a5c0ee4eb732e3cd8f1c39d
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-11-13 14:07:08 -0600

    Update page.php

--- view/students-chart/page.php
+++ view/students-chart/page.php
@@ -1,1 +1,35 @@
 <h1>Students Chart</h1>
+<div>
+  <canvas id="myChart"></canvas>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+    datasets: [{
+        data: [
+          <?php 
+  while ($student = $students->fetch_assoc()) {
+  echo $student[ 'num_enrollments'] . ", ";
+  }    
+?>
+        ]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+   <?php 
+$students = selectStudents();
+  while ($student = $students->fetch_assoc()) {
+  echo "'" . $student[ 'Student_FirstName'] . "', ";
+  }    
+?>
+    ]
+},
+  });
+</script>
+     
