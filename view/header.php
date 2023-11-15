835c6a7e1a3e2e11251c8769d1f8264267000d2d
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-11-15 09:52:30 -0600

    Update header.php

--- view/header.php
+++ view/header.php
@@ -34,6 +34,9 @@
         <li class="nav-item">
           <a class="nav-link" href="students-chart.php">Students Chart</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="majors-bar.php">Majors Chart</a>
+        </li>
           </ul>
     </div>
   </div>
