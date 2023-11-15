4ecc068eab40905f4126f27ff36d74c5b82a6294
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-11-15 12:21:27 -0600

    Update page.php

--- view/division-chart/page.php
+++ view/division-chart/page.php
@@ -1,20 +1,33 @@
+<?php
+  $majors = selectMajors();
+  $data = array();
+  while ($major = $majors->fetch_assoc()) {
+    $data[] = array($major['division'], $major['num_majors']);
+  }
+?>
+
 <html>
   <head>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+      // Ensure the data is defined globally
+      var chartData = <?php echo json_encode($data); ?>;
+      
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
 
-function drawChart() {
-  var data = google.visualization.arrayToDataTable(<?php echo json_encode($data); ?>);
+      function drawChart() {
+        // Use the global variable for data
+        var data = google.visualization.arrayToDataTable(chartData);
 
-  var options = {
-    title: 'Majors by Division Distribution'
-  };
+        var options = {
+          title: 'Majors by Division Distribution'
+        };
 
-  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-  chart.draw(data, options);
-}
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+        chart.draw(data, options);
+      }
     </script>
   </head>
   <body>
