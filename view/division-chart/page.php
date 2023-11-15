92ed980861044cb336feed07d9dce23168f2c794
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-11-15 13:06:04 -0600

    Update page.php

--- view/division-chart/page.php
+++ view/division-chart/page.php
@@ -1,36 +1,35 @@
-<?php
-  $majors = selectMajors();
-  $data = array();
+<h1>Major's Distribution by Division</h1>
+<div>
+  <canvas id="myChart"></canvas>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'pie',
+    data: {
+    datasets: [{
+        data: [
+          <?php 
   while ($major = $majors->fetch_assoc()) {
-    $data[] = array($major['division'], $major['num_majors']);
-  }
+  echo $major[ 'num_majors'] . ", ";
+  }    
 ?>
+        ]
+    }],
 
-<html>
-  <head>
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-      // Ensure the data is defined globally
-      var chartData = <?php echo json_encode($data); ?>;
-      
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
-
-      function drawChart() {
-        // Use the global variable for data
-        var data = google.visualization.arrayToDataTable(chartData);
-
-        var options = {
-          title: 'Majors by Division Distribution'
-        };
-
-        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-        chart.draw(data, options);
-      }
-    </script>
-  </head>
-  <body>
-    <div id="piechart" style="width: 900px; height: 500px;"></div>
-  </body>
-</html>
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+   <?php 
+$majors = selectMajors();
+  while ($major = $majors->fetch_assoc()) {
+  echo "'" . $major[ 'division'] . "', ";
+  }    
+?>
+    ]
+},
+  });
+</script>
+     
