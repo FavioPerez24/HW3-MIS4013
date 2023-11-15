c1b8522c2c2337f0dbd1966c7cdd6228bb6efecd
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-11-15 10:44:42 -0600

    Update page.php

--- view/majors-bar/page.php
+++ view/majors-bar/page.php
@@ -1,46 +1,24 @@
 <h1>Majors Chart</h1>
 <div>
-{
-  // Declare the chart dimensions and margins.
-  const width = 640;
-  const height = 400;
-  const marginTop = 20;
-  const marginRight = 20;
-  const marginBottom = 30;
-  const marginLeft = 40;
 
-  // Declare the x (horizontal position) scale.
-  const x = d3.scaleUtc()
-      .domain([ 
+  
+const stackedBar = new Chart(ctx, {
+    type: 'bar',
+    data: data,
+    options: {
+        scales: {
+            x: {
+                stacked: true
+            },
+            y: {
+                stacked: true
+            }
+        }
+    }
+});
+
 <?php 
   while ($major = $majors->fetch_assoc()) {
   echo $major[ 'num_enrollments'] . ", ";
   }    
 ?>
-])
-      .range([marginLeft, width - marginRight]);
-
-  // Declare the y (vertical position) scale.
-  const y = d3.scaleLinear()
-      .domain([0, 10])
-      .range([height - marginBottom, marginTop]);
-
-  // Create the SVG container.
-  const svg = d3.create("svg")
-      .attr("width", width)
-      .attr("height", height);
-
-  // Add the x-axis.
-  svg.append("g")
-      .attr("transform", `translate(0,${height - marginBottom})`)
-      .call(d3.axisBottom(x));
-
-  // Add the y-axis.
-  svg.append("g")
-      .attr("transform", `translate(${marginLeft},0)`)
-      .call(d3.axisLeft(y));
-
-  // Return the SVG element.
-  return svg.node();
-}
-</div>
