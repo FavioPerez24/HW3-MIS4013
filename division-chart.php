beab646a5ef401f46e43000a37eecaf7a66bf27f
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-11-15 11:57:55 -0600

    Update division-chart.php

--- division-chart.php
+++ division-chart.php
@@ -1,1 +1,10 @@
+<?php
+require_once("model/connection.php");
+require_once("model/division-chart-db.php");
 
+$pageTitle= "Division Chart";
+Include "view/header.php";
+$majors = selectMajors();
+Include "view/division-chart/page.php";
+Include "view/footer.php";
+?>
