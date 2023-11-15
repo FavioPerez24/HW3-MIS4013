228a1778ba45720de8333275bdcc5f6bd16eaf38
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-11-15 09:58:44 -0600

    Create majors-bar-db.php

---
+++ model/majors-bar-db.php
@@ -0,0 +1,15 @@
+<?php
+function selectMajors() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT major_name, division, program_code, minimum_credit_hours FROM `Business_Major`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
