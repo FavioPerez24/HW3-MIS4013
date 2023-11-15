70597b6e543f7ababeecc0cc7ea2703ed088c9dc
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-11-15 10:04:06 -0600

    Update majors-bar-db.php

--- model/majors-bar-db.php
+++ model/majors-bar-db.php
@@ -2,7 +2,7 @@
 function selectMajors() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT major_name, division, program_code, minimum_credit_hours FROM `Business_Major`");
+        $stmt = $conn->prepare("SELECT major_name, count(E.Enrollment_ID) as num_enrollments FROM `Business_Major` M join `Enrollment` E on E.program_code=M.program_code group by major_name");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
