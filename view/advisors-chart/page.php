a4a5cd95a5dcb837495031634021c01927f583cc
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-11-15 11:52:43 -0600

    Create page.php

---
+++ view/advisors-chart/page.php
@@ -0,0 +1,7 @@
+<h1>Advisor Distribution</h1>
+<!-- Load c3.css -->
+<link href="/path/to/c3.css" rel="stylesheet">
+
+<!-- Load d3.js and c3.js -->
+<script src="/path/to/d3.v5.min.js" charset="utf-8"></script>
+<script src="/path/to/c3.min.js"></script>
