5e5c6f75f3572d846062f8f937594b6bb412ef5c
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-11-15 11:32:10 -0600

    Update graduation-line-db.php

--- model/graduation-line-db.php
+++ model/graduation-line-db.php
@@ -1,1 +1,15 @@
-
+<?php
+function selectStudents() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT Graduation_Year, count(E.Enrollment_ID) as num_grad FROM `Student` S join `Enrollment` E on E.Student_ID=S.Student_ID group by Graduation_Year");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
