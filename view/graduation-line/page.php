9b8a53b1f3f78e27e1446fc88164fb2020328883
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-11-15 11:33:57 -0600

    Update page.php

--- view/graduation-line/page.php
+++ view/graduation-line/page.php
@@ -1,1 +1,35 @@
+<h1>Graduation Statistics</h1>
+<div>
+  <canvas id="myChart"></canvas>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
+<script>
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+    datasets: [{
+        data: [
+          <?php 
+  while ($student = $students->fetch_assoc()) {
+  echo $student[ 'num_grad'] . ", ";
+  }    
+?>
+        ]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: [
+   <?php 
+$students = selectStudents();
+  while ($student = $students->fetch_assoc()) {
+  echo "'" . $student[ 'Graduation_Year'] . "', ";
+  }    
+?>
+    ]
+},
+  });
+</script>
+     
