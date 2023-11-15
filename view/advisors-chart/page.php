2e2562f83906ef90d44642464a3cfb88e212af3d
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-11-15 11:55:31 -0600

    Update page.php

--- view/advisors-chart/page.php
+++ view/advisors-chart/page.php
@@ -5,3 +5,15 @@
 <!-- Load d3.js and c3.js -->
 <script src="/path/to/d3.v5.min.js" charset="utf-8"></script>
 <script src="/path/to/c3.min.js"></script>
+
+<div id="chart"></div>
+
+var chart = c3.generate({
+    bindto: '#chart',
+    data: {
+      columns: [
+        ['data1', 30, 200, 100, 400, 150, 250],
+        ['data2', 50, 20, 10, 40, 15, 25]
+      ]
+    }
+});
