14c74221ca25403ff1fddedf5f7ac12a11a6a832
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-11-15 10:50:56 -0600

    Update page.php

--- view/majors-bar/page.php
+++ view/majors-bar/page.php
@@ -2,26 +2,28 @@
 <div>
   <canvas id="myChart"></canvas>
 </div>
+
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 <script>
-const stackedBar = new Chart(ctx, {
-    type: 'bar',
-    data: data,
-    options: {
-        scales: {
-            x: { 
-              <?php 
-  while ($major = $majors->fetch_assoc()) {
-  echo $major[ 'num_enrollments'] . ", ";
-  }    
-?>
-            },
-            y: {1,2,3,4,5,6,7,8,9,10
-                stacked: true
-            }
-        }
-    }
-});
+  const ctx = document.getElementById('myChart');
 
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
 </script>
