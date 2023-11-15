dc3fa97dc0c674e0af02b5dbcbe3b60b3fa06974
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-11-15 12:18:25 -0600

    Update page.php

--- view/division-chart/page.php
+++ view/division-chart/page.php
@@ -2,33 +2,22 @@
   <head>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     <script type="text/javascript">
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
 
-      function drawChart() {
+function drawChart() {
+  var data = google.visualization.arrayToDataTable(<?php echo json_encode($data); ?>);
 
-        var data = google.visualization.arrayToDataTable([
-<?php
-  $majors = selectMajors();
-  $data = array();
-  while ($major = $majors->fetch_assoc()) {
-    $data[] = array($major['division'], $major['num_majors']);
-  }
-?>
-        ]);
+  var options = {
+    title: 'Majors by Division Distribution'
+  };
 
-        var options = {
-          title: 'Majors by Division Distribution'
-        };
-
-        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-        chart.draw(data, options);
-      }
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+  chart.draw(data, options);
+}
     </script>
   </head>
   <body>
     <div id="piechart" style="width: 900px; height: 500px;"></div>
   </body>
 </html>
-
