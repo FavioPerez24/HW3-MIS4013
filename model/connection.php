d4888e2fc46816fad4919a280f27a735842841f4
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-02 20:36:05 -0600

    Update connection.php

--- model/connection.php
+++ model/connection.php
@@ -2,7 +2,7 @@
 function get_db_connection(){
     // Create connection
     // You can use Local Host
-    $conn = new mysqli('159.89.47.44', 'fpgarcia_HW3user', 'Q*cywhRWb+Di', 'fpgarcia_HW3');
+    $conn = new mysqli('159.89.47.44', 'fpgarcia_projectuser', 'wiZaK~1p*S#3', 'fpgarcia_project');
     
     // Check connection
     if ($conn->connect_error) {
