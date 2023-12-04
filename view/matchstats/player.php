5f3ce8c80eca94363e2492f9135effe65a7b3f7e
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-04 17:22:58 -0600

    Update and rename student.php to player.php

---
+++ view/matchstats/player.php
@@ -0,0 +1,18 @@
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $player['PName']; ?></h5>
+                    <p class="card-text">
+                        <ul class="list-group">
+                            <?php
+                            $matches = selectPlayerByMatch($player['PID']);
+                            while ($match = $matches->fetch_assoc()) {
+                              include "match.php";
+                           } 
+                          ?>
+                        </ul>
+                    </p>
+                    <p class="card-text"><small class="text-body-secondary">Date of Birth: <?php echo $player['PDOB']; ?></small></p>
+                </div>
+            </div>
+        </div>
