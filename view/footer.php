627ea89023e79f0d63fd337cea2710b89789f4e4
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-04 10:45:05 -0600

    Update footer.php

--- view/footer.php
+++ view/footer.php
@@ -1,4 +1,18 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>The UEFA Collection</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+body {font-family: "Lato", sans-serif}
+.mySlides {display: none}
+</style>
+</head>
+<body>
 <footer class="w3-container w3-padding-64 w3-center w3-black w3-xlarge">
   <a href="#"><i class="fa fa-facebook-official"></i></a>
   <a href="#"><i class="fa fa-pinterest-p"></i></a>
@@ -9,4 +23,4 @@
   Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a>
   </p>
 </footer>
-</html>
+</body>
