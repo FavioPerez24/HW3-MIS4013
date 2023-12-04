f42f4b522168dbd383d64b6ecb49db2c5637ba01
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-03 18:35:57 -0600

    Update and rename students.php to players.php

---
+++ players.php
@@ -0,0 +1,37 @@
+<?php
+require_once("model/connection.php");
+require_once("model/player-db.php");
+
+$pageTitle= "Players";
+Include "view/header.php";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertPlayer($_POST['sFName'], $_POST['sGrad'], $_POST['aid'])) {
+        echo '<div class="alert alert-success" role="alert">Player added.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Edit":
+      if (updatePlayer($_POST['sFName'], $_POST['sGrad'], $_POST['aid'], $_POST['sid'])) {
+        echo '<div class="alert alert-success" role="alert">Player edited.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Delete":
+      if (deletePlayer($_POST['sid'])) {
+        echo '<div class="alert alert-success" role="alert">Player deleted.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+  }
+}
+
+$players = selectPlayers();
+Include "view/players/page.php";
+Include "view/footer.php";
+?>
