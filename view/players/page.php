270cc2591afd02c07756e326ff44ee307477f451
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-04 13:09:59 -0600

    Update page.php

--- view/players/page.php
+++ view/players/page.php
@@ -8,10 +8,6 @@
   </head>
   <body>
     
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-  </body>
-</html>
-
 <div class="row">
   <div class="col">
 <h1>Our Players</h1>
@@ -60,8 +56,8 @@ include "new-form.php";
         </button>
       </form>
     </td>
-        <td><a href="student-by-advisor.php?id=<?php echo $student['Student_ID']; ?>">Advisor</a></td>
-        <td><a href="student-by-major.php?id=<?php echo $student['Student_ID']; ?>">Major</a></td>
+        <td><a href="player-by-team.php?id=<?php echo $player['PID']; ?>">Team</a></td>
+        <td><a href="player-by-match.php?id=<?php echo $player['PID']; ?>">Matches</a></td>
       </tr>
       <?php
       }
@@ -69,3 +65,6 @@ include "new-form.php";
     </tbody>
   </table>
 </div>
+ <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
