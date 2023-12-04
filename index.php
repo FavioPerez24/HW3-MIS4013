bb7ad98aa91f93f93dc593e660c3ec1f0e546db5
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-03 23:08:58 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,14 +5,17 @@ Include "view/header.html";
 <!DOCTYPE html>
 <html>
 <head>
-	<script src="https://unpkg.com/scrollreveal@4"></script>
-	<script>
-		ScrollReveal({ duration: 1000 })
-	</script>
+<script src="https://unpkg.com/scrollreveal@4"></script>
+<script>
+	ScrollReveal({ duration: 1000 })
+</script>
+<style>
+	h1.headline {text-align: center;}
+	p.tagline {text-align: center;}
+	.punchline {display: inline-block;}
+</style>
 </head>
 <body>
-
-
 	<h1 class="headline">
 		The UEFA Collection.
 	</h1>
@@ -20,7 +23,6 @@ Include "view/header.html";
 		Where data meets talents
 		<span class="punchline"> ...Get in touch...</span>
 	</p>
-
 	<script>
 		ScrollReveal().reveal('.headline')
 		ScrollReveal().reveal('.tagline', { delay: 500 })
