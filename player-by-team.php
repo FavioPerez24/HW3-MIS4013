03e653da60de035bc74f0fff7f703dadf99b6117
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-04 09:52:52 -0600

    Update and rename student-by-advisor.php to player-by-team.php

---
+++ player-by-team.php
@@ -0,0 +1,37 @@
+<?php
+require_once("model/connection.php");
+require_once("model/player-by-team-db.php");
+
+$pageTitle= "Player's Team";
+Include "view/header.html";
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertTeam($_POST['Tname'], $_POST['Tcountry'], $_POST['Tcoach'])) {
+        echo '<div class="alert alert-success" role="alert">New Team added.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Edit":
+      if (updateTeam($_POST['Tname'], $_POST['Tcountry'], $_POST['Tcoach'], $_POST['Tid'])) {
+        echo '<div class="alert alert-success" role="alert">Team edited.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+    case "Delete":
+      if (deleteTeam($_POST['Tid'])) {
+        echo '<div class="alert alert-success" role="alert">Team deleted.</div>';
+      } else {
+        echo '<div class="alert alert-danger" role="alert">Error.</div>';
+      }
+      break;
+  }
+}
+
+$teams = selectPlayerbyTeam($_GET['id']);
+Include "view/players/byteams/page.php";
+Include "view/footer.php";
+?>
