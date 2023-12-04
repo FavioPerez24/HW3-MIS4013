e45653ad1d112265d47e0a975c048bd58bde8c84
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-04 10:37:43 -0600

    Update player-by-match.php

--- player-by-match.php
+++ player-by-match.php
@@ -3,7 +3,7 @@ require_once("model/connection.php");
 require_once("model/player-by-match-db.php");
 
 $pageTitle= "Player by Match";
-Include "view/header.html";
+Include "view/header.php";
 $matchs = selectPlayerbyMatch($_GET['id']);
 Include "view/players-by-match.php";
 Include "view/footer.php";
