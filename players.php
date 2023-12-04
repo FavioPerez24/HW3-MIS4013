5603c34c079254e8b0a3f8805c71ca8803b54597
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-04 08:06:36 -0600

    Update players.php

--- players.php
+++ players.php
@@ -2,20 +2,20 @@
 require_once("model/connection.php");
 require_once("model/players-db.php");
 
-$pageTitle= "Players";
+$pageTitle= "Our Players";
 Include "view/header.php";
 
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertPlayer($_POST['$PName'],['$PNat'], ['$Tid'])) {
+      if (insertPlayer($_POST['$PName'],['$PDob'], ['$PNat'],['$PPos'], ['$Tid'])) {
         echo '<div class="alert alert-success" role="alert">Player added.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
     case "Edit":
-      if (updatePlayer($_POST['$PName'],['$PNat'], ['$Tid'], $_POST['Pid'])) {
+      if (updatePlayer($_POST['$PName'],['$PDob'],['$PNat'],['$PPos'],['$Tid'], $_POST['Pid'])) {
         echo '<div class="alert alert-success" role="alert">Player edited.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
