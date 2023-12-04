b97c3b34783d9f9d30646fb6a014e6b98c64ae02
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-03 23:23:49 -0600

    Update index.php

--- index.php
+++ index.php
@@ -10,9 +10,9 @@ Include "view/header.html";
 	ScrollReveal({ duration: 1000 })
 </script>
 <style>
-	h1.headline {text-align: center; animation: repeatText 3s linear infinite;}
-	p.tagline {text-align: center; animation: repeatText 3s linear infinite;}
-	.punchline {display: inline-block; animation: repeatText 3s linear infinite;}
+	h1.headline {text-align: center;}
+	p.tagline {text-align: center; animation: repeatText 4s linear infinite;}
+	.punchline {display: inline-block; animation: repeatText 2s linear infinite;}
 	body {font-family: "Lato", sans-serif}
 	@keyframes repeatText {
 			0% { opacity: 1; }
@@ -28,12 +28,12 @@ Include "view/header.html";
 	</h1>
 	<p class="tagline">
 		Where data meets talents
-		<span class="punchline"> ...Get in touch...</span>
+		<span class="punchline"> ... Get in touch ...</span>
 	</p>
 	<script>
 		ScrollReveal().reveal('.headline')
-		ScrollReveal().reveal('.tagline', { delay: 250 })
-		ScrollReveal().reveal('.punchline', { delay: 500 })
+		ScrollReveal().reveal('.tagline', { delay: 400 })
+		ScrollReveal().reveal('.punchline', { delay: 750 })
 	</script>
 </body>
 </html><?php
