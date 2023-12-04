a67707e091fa78f1eac35a402e845b32dd986544
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-04 17:30:42 -0600

    Update and rename major-input-list.php to match-input-list.php

---
+++ view/matchstats/match-input-list.php
@@ -0,0 +1,13 @@
+<select class="form-select" id="Mid" name="Mid">
+<?php
+while ($matchItem = $matchList->fetch_assoc()) {
+  $selText = "";
+  if ($selectedMatch == $matchItem['MID']) {
+    $selText = " selected";
+  }
+?>
+  <option value="<?php echo $matchItem['MID']; ?>"<?=$selText?>><?php echo $matchItem['MDetails']; ?></option>
+<?php
+}
+?>
+</select>
