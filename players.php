cc2482383a593084a332c29bd0d6c5055367eea4
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-04 08:47:58 -0600

    Update players.php

--- players.php
+++ players.php
@@ -8,14 +8,14 @@ Include "view/header.html";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertPlayer($_POST['$PName'],['$PDob'], ['$PNat'],['$PPos'], ['$Tid'])) {
+      if (insertPlayer($_POST['$Pname'],['$Pdob'], ['$Pnat'],['$Ppos'], ['$Tid'])) {
         echo '<div class="alert alert-success" role="alert">Player added.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
     case "Edit":
-      if (updatePlayer($_POST['$PName'],['$PDob'],['$PNat'],['$PPos'],['$Tid'], $_POST['Pid'])) {
+      if (updatePlayer($_POST['$Pname'],['$Pdob'],['$Pnat'],['$Ppos'],['$Tid'], $_POST['Pid'])) {
         echo '<div class="alert alert-success" role="alert">Player edited.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
