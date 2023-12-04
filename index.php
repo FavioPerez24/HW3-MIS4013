b7dcc276608444c5858a1c69f701231337bc10e7
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-04 10:49:43 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,23 @@
 $pageTitle= "Home";
 Include "view/header.php";
 ?>
-    <h1>Project</h1>
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://unpkg.com/scrollreveal@4"></script>
+<style>
+	h1.headline {text-align: center;}
+	p.tagline {text-align: center; animation: repeatText 5s linear infinite;}
+	.punchline {display: inline-block; animation: repeatText 5s linear infinite;}
+	body {font-family: "Lato", sans-serif}
+	@keyframes repeatText {
+			0% { opacity: 1; }
+			50% { opacity: 0; }
+			100% { opacity: 1; }
+		}
+</style>
+
+
 <?php
 Include "home.html";
 ?>
