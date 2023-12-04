6a1cff1a7a2f9c7da2a3ae50cc49aaa2ea92b24f
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-04 08:23:50 -0600

    Update matchstats.php

--- matchstats.php
+++ matchstats.php
@@ -8,14 +8,14 @@ Include "view/header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertMatchStats($_POST['Msid'], $_POST['mid'])) {
+      if (insertMatchStats($_POST['Mid'], $_POST['Pid'], $_POST['Goal'], $_POST['Shoots'], $_POST['Passes'], $_POST['Chances'], $_POST['Miles'])) {
         echo '<div class="alert alert-success" role="alert">Statistics added.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
     case "Edit":
-      if (updateMatchStats($_POST['sid'], $_POST['mid'], $_POST['eid'])) {
+      if (updateMatchStats($_POST['Mid'], $_POST['Pid'], $_POST['Goal'], $_POST['Shoots'], $_POST['Passes'], $_POST['Chances'], $_POST['Miles'], $_POST['Msid'])) {
         echo '<div class="alert alert-success" role="alert">Statistics edited.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
