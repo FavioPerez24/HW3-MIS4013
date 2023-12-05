6890f5809fb9b592b4ea62535413c4ea0b0ef571
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-05 13:35:19 -0600

    Update match.php

--- view/matchstats/match.php
+++ view/matchstats/match.php
@@ -7,7 +7,7 @@
                                         </div>
                                         <div class="col-auto">
                                             <form method="post" action="">
-                                                <input type="hidden" name="Msid" value="<?php echo $match['MSID']; ?>">
+                                                <input type="hidden" name="Msid" value="<?php echo $match['Msid']; ?>">
                                                 <input type="hidden" name="actionType" value="Delete">
                                                 <button type="submit" class="btn" onclick="return confirm('Are you sure?');">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
