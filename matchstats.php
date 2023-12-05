8bfe72e405d6e08f2ff54ba50ef4f1b787b60cb2
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-04 22:32:25 -0600

    Update matchstats.php

--- matchstats.php
+++ matchstats.php
@@ -8,21 +8,21 @@ Include "view/header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertMatchStats($_POST['Mid'], $_POST['Pid'], $_POST['Goal'], $_POST['Shoots'], $_POST['Passes'], $_POST['Chances'], $_POST['Miles'])) {
+      if (insertMatchStat($_POST['Mid'], $_POST['Pid'], $_POST['Goal'], $_POST['Shoots'], $_POST['Passes'], $_POST['Chances'], $_POST['Miles'])) {
         echo '<div class="alert alert-success" role="alert">Statistics added.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
     case "Edit":
-      if (updateMatchStats($_POST['Mid'], $_POST['Pid'], $_POST['Goal'], $_POST['Shoots'], $_POST['Passes'], $_POST['Chances'], $_POST['Miles'], $_POST['Msid'])) {
+      if (updateMatchStat($_POST['Mid'], $_POST['Pid'], $_POST['Goal'], $_POST['Shoots'], $_POST['Passes'], $_POST['Chances'], $_POST['Miles'], $_POST['Msid'])) {
         echo '<div class="alert alert-success" role="alert">Statistics edited.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
       }
       break;
     case "Delete":
-      if (deleteMatchStats($_POST['Msid'])) {
+      if (deleteMatchStat($_POST['Msid'])) {
         echo '<div class="alert alert-success" role="alert">Stats deleted.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
