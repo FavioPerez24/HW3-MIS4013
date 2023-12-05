a6e7e368e68217c57fa43ee230cb3c443bd5f5b5
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-04 22:48:23 -0600

    Update player-by-match-db.php

--- model/player-by-match-db.php
+++ model/player-by-match-db.php
@@ -2,7 +2,7 @@
 function selectPlayerbyMatch($Pid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT M.MID, MDetails, MDate, MConditions FROM `MatchGame` M JOIN MatchStats MS ON M.MID=MS.MID WHERE MS.PID=?");
+        $stmt = $conn->prepare("SELECT M.MID, P.PID, P.PName, P.PNationality FROM MatchGame M JOIN MatchStats MS ON M.MID = MS.MID JOIN Player P ON P.PID = MS.PID WHERE M.MID = ?");
         $stmt->bind_param("i", $Pid);
         $stmt->execute();
         $result = $stmt->get_result();
