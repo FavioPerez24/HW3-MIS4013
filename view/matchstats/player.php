4062a199f7e1e9ea33f7e82d4c2e98a2109858be
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-05 11:51:58 -0600

    Update player.php

--- view/matchstats/player.php
+++ view/matchstats/player.php
@@ -1,4 +1,13 @@
-        <div class="col">
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+<div class="col">
            <div class="card" style="width: 18rem;">
         <a href="https://www.tsn.ca/content/dam/tsn/en/home/images/2023/11/4/jeremy-doku--l--1-2031006-1699122086615.jpeg">
             <img src="https://www.tsn.ca/content/dam/tsn/en/home/images/2023/11/4/jeremy-doku--l--1-2031006-1699122086615.jpeg" class="card-img-top" alt="Player Image">
@@ -19,3 +28,5 @@
                 </div>
             </div>
         </div>
+  </body>
+</html>
