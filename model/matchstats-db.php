3c81a050c68d06e92482bfddbb97aac2e50d5587
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-05 15:27:43 -0600

    Update matchstats-db.php

--- model/matchstats-db.php
+++ model/matchstats-db.php
@@ -15,7 +15,7 @@ function selectPlayers() {
 function selectMatchbyPlayer($Pid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT M.MID, M.MDate, Goals_Scored, Shoots, Passes_Completed, Chances_Created, Miles_Run FROM MatchGame M JOIN MatchStats MS ON M.MID = MS.MID WHERE MS.PID = ?");
+        $stmt = $conn->prepare("SELECT M.MID, M.MDetails, Goals_Scored, Shoots, Passes_Completed, Chances_Created, Miles_Run FROM MatchGame M JOIN MatchStats MS ON M.MID = MS.MID WHERE MS.PID = ?");
         $stmt->bind_param("i", $Pid);
         $stmt->execute();
         $result = $stmt->get_result();
