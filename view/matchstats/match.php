6b16095441031604b148255274db13710def9953
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-04 20:56:22 -0600

    Update match.php

--- view/matchstats/match.php
+++ view/matchstats/match.php
@@ -1,7 +1,7 @@
                                 <li class="list-group-item">
                                     <div class="row">
                                         <div class="col">
-                                            <?php echo $match['MDetails']; ?> - <?php echo $match['MDate']; ?> - <?php echo $match['MConditions']; ?>
+                                            <?php echo $match['Mdet']; ?> - <?php echo $match['Mdat']; ?> - <?php echo $match['Mcond']; ?>
                                         </div>
                                         <div class="col-auto">
                                             <?php include "edit-form.php"; ?>
