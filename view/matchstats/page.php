54f4967b9276648778b3070c79a1865d5a572ea6
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-05 11:49:52 -0600

    Update page.php

--- view/matchstats/page.php
+++ view/matchstats/page.php
@@ -1,25 +1,13 @@
-<?php
-function getPlayers() {
-    // Fetch players from the database and return them as an array
-}
-
-function generatePlayerHtml($player) {
-    // Generate the HTML for a single player
-}
-
-$players = getPlayers();
-?>
-
 <!doctype html>
 <html lang="en">
- <head>
+  <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
- </head>
- <body>
-      
+  </head>
+  <body>
+
 <div class="row">
     <div class="col">
         <h1>Statistics</h1>
@@ -31,19 +19,17 @@ $players = getPlayers();
 <div class="card-group">
     <?php
     $count = 0;
-    foreach ($players as $player) {
+    while ($player = $players->fetch_assoc()) {
         if ($count % 2 == 0) {
             echo '<div class="row">';
-        }
-        
-        echo generatePlayerHtml($player);
-        
-        if ($count % 2 !== 0) {
+        }        
+                include "player.php";
+        if ($count % 2 !== 0 || $players->num_rows % 2 !== 0) {
             echo '</div>';
         }
         $count++;
     }
     ?>
 </div>
- </body>
+  </body>
 </html>
