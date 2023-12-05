9289f6192abc0d7bf266b8b8fad85c3bc7e3a225
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-05 16:42:51 -0600

    Update goals-chart-db.php

--- model/goals-chart-db.php
+++ model/goals-chart-db.php
@@ -2,7 +2,7 @@
 function selectStats() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT P.PID, P.PName, SUM(Goals_Scored) AS Total_goals, COUNT(M.MID) AS Total_matches FROM Player P LEFT JOIN MatchStats MS ON P.PID = MS.PID LEFT JOIN MatchGame M ON MS.MID= M.MID GROUP BY P.PID, P.PName");
+        $stmt = $conn->prepare("SELECT P.PID, P.PName AS Player_Name, SUM(Goals_Scored) AS Total_goals, COUNT(M.MID) AS Total_matches FROM Player P LEFT JOIN MatchStats MS ON P.PID = MS.PID LEFT JOIN MatchGame M ON MS.MID= M.MID GROUP BY P.PID, P.PName");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
