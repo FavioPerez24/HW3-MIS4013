98205bde601bf5006984396005f1eeeefbd35240
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-05 00:43:53 -0600

    Update player.php

--- view/matchstats/player.php
+++ view/matchstats/player.php
@@ -1,5 +1,8 @@
         <div class="col">
-            <div class="card">
+           <div class="card" style="width: 18rem;">
+        <a href="https://www.tsn.ca/content/dam/tsn/en/home/images/2023/11/4/jeremy-doku--l--1-2031006-1699122086615.jpeg">
+            <img src="https://www.tsn.ca/content/dam/tsn/en/home/images/2023/11/4/jeremy-doku--l--1-2031006-1699122086615.jpeg" class="card-img-top" alt="Player Image">
+        </a>
                 <div class="card-body">
                     <h5 class="card-title"><?php echo $player['PName']; ?></h5>
                     <p class="card-text">
