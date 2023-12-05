b70741ce0f7944c3114e024d372fbbb518ecb49c
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-05 15:39:36 -0600

    Update matchstats.php

--- matchstats.php
+++ matchstats.php
@@ -31,7 +31,7 @@ if (isset($_POST['actionType'])) {
   }
 }
 
-$matches = selectMatchbyPlayer($_GET['Msid']);
+$matches = selectMatchbyPlayer($_GET['Mid']);
 Include "view/matchstats/page1.php";
 Include "view/footer.php";
 ?>
