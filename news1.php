b9787fadde04f0a95de08fe484728a070c33ad18
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-05 21:27:39 -0600

    Update news1.php

--- news1.php
+++ news1.php
@@ -8,17 +8,17 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 <style>
         .all-browsers {
-            background-color: cream;
+            background-color: white;
             /* Add any other styles you want for the section */
             padding: 20px; /* Example padding */
         }
    .browsers {
-            background-color: cream;
+            background-color: blue;
             /* Add any other styles you want for the section */
             padding: 20px; /* Example padding */
         }
   .browsers-section {
-            background-color: cream;
+            background-color: red;
             /* Add any other styles you want for the section */
             padding: 20px; /* Example padding */
         }
