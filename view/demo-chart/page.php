d5706074385e0664a54c3ac78ee71968b1f69f74
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-06 12:10:51 -0600

    Update page.php

--- view/demo-chart/page.php
+++ view/demo-chart/page.php
@@ -7,30 +7,26 @@
 <script>
   const ctx = document.getElementById('myChart');
 
+  <?php
+  $stats = selectStats();
+  $labels = [];
+  $data = [];
+
+  while ($stat = $stats->fetch_assoc()) {
+    $labels[] = "'" . $stat['PNationalities'] . "'";
+    $data[] = $stat['PlayersbyNat'];
+  }
+  ?>
+
   new Chart(ctx, {
     type: 'line',
     data: {
-    datasets: [{
-      label: 'Countries',  
-      data: [
-          <?php 
-  while ($player = $players->fetch_assoc()) {
-  echo $player[ 'PlayersbyNat'] . ", ";
-  }    
-?>
-        ]
-    }],
-
-    // These labels appear in the legend and in the tooltips when hovering different arcs
-    labels: [
-   <?php 
-$players = selectPlayers();
-  while ($player = $players->fetch_assoc()) {
-  echo "'" . $player[ 'PNationality'] . "', ";
-  }    
-?>
-    ]
-},
+      labels: [<?php echo implode(', ', $labels); ?>],
+      datasets: [{
+        label: 'Countries',
+        data: [<?php echo implode(', ', $data); ?>],
+      }],
+    },
   });
 </script>
      
