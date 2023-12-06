67622650c6128ac6b5f45d74f3d929fa1b6052a2
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-05 18:40:08 -0600

    Update new-form.php

--- view/playersbyteam/new-form.php
+++ view/playersbyteam/new-form.php
@@ -48,5 +48,9 @@ include "team-input-list.php";
     </div>
   </div>
 </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
   </body>
 </html>
