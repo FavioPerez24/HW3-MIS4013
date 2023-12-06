becb9195e7428f51d72e111131756710eeef3185
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-06 00:31:50 -0600

    Update page.php

--- view/compare-chart/page.php
+++ view/compare-chart/page.php
@@ -4,9 +4,7 @@
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Players Polar Area Chart</title>
-   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
-<body>
    <div>
   <canvas id="myChart"></canvas>
 </div>
