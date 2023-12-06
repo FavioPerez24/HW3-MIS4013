5da67bebec3189489f07b98e3290e1f32fc14126
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-06 10:58:41 -0600

    Update compare-chart-db.php

--- model/compare-chart-db.php
+++ model/compare-chart-db.php
@@ -2,7 +2,7 @@
 function selectPlayers() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT PID, PName, PPosition FROM `Player`");
+        $stmt = $conn->prepare("SELECT PID, PName AS Player_Name, PPosition as Position FROM `Player`");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
@@ -16,7 +16,7 @@ function selectPlayers() {
 function selectStats($sid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT MSID, Goals_Scored, Shoots, Passes_Completed, Chances_Created, Miles_Run FROM MatchGame M JOIN MatchStats MS ON M.MID = MS.MID WHERE MS.PID = ?");
+        $stmt = $conn->prepare("SELECT SUM(Goals_Scored) AS Total_goals, SUM(Shoots) AS Total_shoots, SUM(Passes_Completed) AS Total_passes, SUM(Chances_Created) AS Total_chances, SUM(Miles_Run) AS Total_miles FROM Player P LEFT JOIN MatchStats MS ON P.PID = MS.PID LEFT JOIN MatchGame M ON MS.MID= M.MID WHERE MS.PID = ?  GROUP BY P.PID, P.PName");
         $stmt->bind_param("i", $id);
         $stmt->execute();
         $result = $stmt->get_result();
