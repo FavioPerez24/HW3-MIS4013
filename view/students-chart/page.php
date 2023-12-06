00682837b12acb4dd100891e07e2d214ba589180
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-05 23:21:13 -0600

    Update page.php

--- view/students-chart/page.php
+++ view/students-chart/page.php
@@ -1,35 +1,39 @@
-<h1>Students Chart</h1>
-<div>
-  <canvas id="myChart"></canvas>
-</div>
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-
-<script>
-  const ctx = document.getElementById('myChart');
-
-  new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-    datasets: [{
-        data: [
-          <?php 
-  while ($student = $students->fetch_assoc()) {
-  echo $student[ 'num_enrollments'] . ", ";
-  }    
-?>
-        ]
-    }],
-
-    // These labels appear in the legend and in the tooltips when hovering different arcs
-    labels: [
-   <?php 
-$students = selectStudents();
-  while ($student = $students->fetch_assoc()) {
-  echo "'" . $student[ 'Student_FirstName'] . "', ";
-  }    
-?>
-    ]
-},
-  });
-</script>
-     
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Player Polar Area Chart</title>
+   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+</head>
+<body>
+   <div style="width:75%">
+      <canvas id="polarChart"></canvas>
+   </div>
+   <script>
+      var ctx = document.getElementById('polarChart').getContext('2d');
+      var chart = new Chart(ctx, {
+         type: 'polarArea',
+         data: {
+            labels: ['Goals', 'Passes', 'Chances', 'Miles', 'Shoots'],
+            datasets: [{
+               label: 'Player A',
+               data: [20, 10, 5, 30, 25],
+               backgroundColor: ['rgba(255, 99, 132, 0.5)']
+            }, {
+               label: 'Player B',
+               data: [15, 8, 4, 25, 20],
+               backgroundColor: ['rgba(75, 192, 192, 0.5)']
+            }]
+         },
+         options: {
+            scale: {
+               ticks: {
+                 beginAtZero: true
+               }
+            }
+         }
+      });
+   </script>
+</body>
+</html>
