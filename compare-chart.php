2546c70c9907f31a16fe314890d7aff1aa72ccda
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-06 10:59:04 -0600

    Update compare-chart.php

--- compare-chart.php
+++ compare-chart.php
@@ -4,7 +4,7 @@ require_once("model/compare-chart-db.php");
 
 $pageTitle= "Performance Comparison Chart";
 Include "view/header.php";
-$stats = selectStats();
+$players = selectPlayers();
 Include "view/compare-chart/page1.php";
 Include "view/footer.php";
 ?>
