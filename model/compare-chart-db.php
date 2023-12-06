30cc9fa2b42297137f6b3e7fe1691f87256f6b1b
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-06 10:54:15 -0600

    Update compare-chart-db.php

--- model/compare-chart-db.php
+++ model/compare-chart-db.php
@@ -13,10 +13,10 @@ function selectPlayers() {
     }
 }
 
-function selectMatchbyPlayer($id) {
+function selectStats($sid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT M.MID, M.MDetails, Goals_Scored, Shoots, Passes_Completed, Chances_Created, Miles_Run FROM MatchGame M JOIN MatchStats MS ON M.MID = MS.MID WHERE MS.PID = ?");
+        $stmt = $conn->prepare("SELECT MSID, Goals_Scored, Shoots, Passes_Completed, Chances_Created, Miles_Run FROM MatchGame M JOIN MatchStats MS ON M.MID = MS.MID WHERE MS.PID = ?");
         $stmt->bind_param("i", $id);
         $stmt->execute();
         $result = $stmt->get_result();
