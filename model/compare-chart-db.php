10c6b918e627ee15cbcd280b0d5fc16a158a7945
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-06 10:52:16 -0600

    Update compare-chart-db.php

--- model/compare-chart-db.php
+++ model/compare-chart-db.php
@@ -1,4 +1,18 @@
 <?php
+function selectPlayers() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT PID, PName FROM `Player`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
 function selectStats() {
     try {
         $conn = get_db_connection();
