5e9df880db5a67b38bf6f7df9ee479391230cbd0
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-05 20:54:43 -0600

    Update news1.php

--- news1.php
+++ news1.php
@@ -1,9 +1,11 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
- <meta charset="UTF-8">
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <link rel="stylesheet" href="styles.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap demo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+<section>
  <title>Top Stories</title>
 </head>
 <body>
@@ -45,6 +47,7 @@
       article.classList.toggle('show');
     }
  </script>
+ </section>
   <section>
    <?php include "news-section.php"; ?>
   </section>
