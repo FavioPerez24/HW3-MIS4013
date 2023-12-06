c7b5345984962af287dd9520f18cc2ddef811239
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-05 23:37:26 -0600

    Update goals-chart.php

--- goals-chart.php
+++ goals-chart.php
@@ -6,5 +6,6 @@ $pageTitle= "Goals per Player";
 Include "view/header.php";
 $stats = selectStats();
 Include "view/goals-chart/page.php";
+Include "view/compare-chart/page.php";
 Include "view/footer.php";
 ?>
