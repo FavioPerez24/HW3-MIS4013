0e762fcf69a92740d8793a916befc4316d8159ae
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-06 12:42:27 -0600

    Update header.php

--- view/header.php
+++ view/header.php
@@ -25,9 +25,9 @@ body {font-family: "Lato", sans-serif}
     <div class="w3-dropdown-hover w3-hide-small">
       <button class="w3-padding-large w3-button" title="More">MORE <i class="fa fa-caret-down"></i></button>     
       <div class="w3-dropdown-content w3-bar-block w3-card-4">
-        <a href="goals-chart.php" class="w3-bar-item w3-button">DATA AND DEMOGRAPHICS</a>
-        <a href="compare-chart.php" class="w3-bar-item w3-button">PERFORMANCE COMPARISON</a>
-        <a href="newspage.php" class="w3-bar-item w3-button">NEWS</a>
+        <a href="goals-chart.php" class="w3-bar-item w3-button">DATA COMPARISON</a>
+        <a href="demo-chart.php" class="w3-bar-item w3-button">PLAYERS DEMOGRAPHICS</a>
+        <a href="news/page.php" class="w3-bar-item w3-button">NEWS</a>
       </div>
     </div>
     <div>
