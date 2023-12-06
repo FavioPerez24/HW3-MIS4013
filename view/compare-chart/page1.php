a2be2b768f3a51fefe5efec13502d74e96b644ea
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-06 01:47:42 -0600

    Update page1.php

--- view/compare-chart/page1.php
+++ view/compare-chart/page1.php
@@ -5,51 +5,60 @@
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script>
-   <?php
-   $stats = selectStats();
-   $playerData = [];
+<?php
+$stats = selectStats();
+$playerData = [];
+?>
 
-   while ($stat = $stats->fetch_assoc()) {
-      $playerName = $stat['Player_Name'];
-      $labels = ['Goals', 'Shoots', 'Passes', 'Chances', 'Miles'];
-      $data = [
-         isset($stat['Total_goals']) ? $stat['Total_goals'] : 0,
-         isset($stat['Total_shoots']) ? $stat['Total_shoots'] : 0,
-         isset($stat['Total_passes']) ? $stat['Total_passes'] : 0,
-         isset($stat['Total_chances']) ? $stat['Total_chances'] : 0,
-         isset($stat['Total_miles']) ? $stat['Total_miles'] : 0,
-      ];
+<?php while ($stat = $stats->fetch_assoc()): ?>
+    <?php
+    $playerName = $stat['Player_Name'];
+    $labels = ['Goals', 'Shoots', 'Passes', 'Chances', 'Miles'];
+    $data = [
+        isset($stat['Total_goals']) ? $stat['Total_goals'] : 0,
+        isset($stat['Total_shoots']) ? $stat['Total_shoots'] : 0,
+        isset($stat['Total_passes']) ? $stat['Total_passes'] : 0,
+        isset($stat['Total_chances']) ? $stat['Total_chances'] : 0,
+        isset($stat['Total_miles']) ? $stat['Total_miles'] : 0,
+    ];
 
-      $playerData[] = [
-         'playerName' => $playerName,
-         'labels' => json_encode($labels),
-         'data' => json_encode($data),
-         'backgroundColor' => 'rgba(255, 99, 132, 0.5)'
-      ];
-   }
-   ?>
+    $playerData[] = [
+        'playerName' => $playerName,
+        'labels' => json_encode($labels),
+        'data' => json_encode($data),
+        'backgroundColor' => 'rgba(255, 99, 132, 0.5)'
+    ];
+    ?>
+<?php endwhile; ?>
 
-   var ctx = document.getElementById('myChart').getContext('2d');
+<?php foreach ($playerData as $index => $data): ?>
+    <div>
+        <h2><?php echo $data['playerName']; ?></h2>
+        <canvas id="myChart<?php echo $index; ?>"></canvas>
+    </div>
 
-   <?php foreach ($playerData as $index => $data): ?>
-      var chartData<?php echo $index; ?> = <?php echo json_encode($data); ?>;
-      var chart<?php echo $index; ?> = new Chart(ctx, {
-         type: 'polarArea',
-         data: {
-            labels: chartData<?php echo $index; ?>.labels,
-            datasets: [{
-               label: chartData<?php echo $index; ?>.playerName,
-               data: chartData<?php echo $index; ?>.data,
-               backgroundColor: chartData<?php echo $index; ?>.backgroundColor
-            }]
-         },
-         options: {
-            scale: {
-               ticks: {
-                  beginAtZero: true
-               }
+    <script>
+        var ctx<?php echo $index; ?> = document.getElementById('myChart<?php echo $index; ?>').getContext('2d');
+        var chartData<?php echo $index; ?> = <?php echo json_encode($data); ?>;
+        var chart<?php echo $index; ?> = new Chart(ctx<?php echo $index; ?>, {
+            type: 'polarArea',
+            data: {
+                labels: chartData<?php echo $index; ?>.labels,
+                datasets: [{
+                    label: chartData<?php echo $index; ?>.playerName,
+                    data: chartData<?php echo $index; ?>.data,
+                    backgroundColor: chartData<?php echo $index; ?>.backgroundColor
+                }]
+            },
+            options: {
+                scale: {
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }
             }
-         }
-      });
-   <?php endforeach; ?>
+        });
+    </script>
+<?php endforeach; ?>
 </script>
+
