5c7efc21fc96f933ebf64562b6d723d9b863bba9
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-05 20:39:06 -0600

    Create news-section.php

---
+++ news-section.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Football News and Updates</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+        }
+        .container {
+            max-width: 960px;
+            margin: 0 auto;
+            padding: 0 20px;
+        }
+        .news-card {
+            background-color: #f1f1f1;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            border-radius: 4px;
+            overflow: hidden;
+            margin-bottom: 20px;
+        }
+        .news-card img {
+            width: 100%;
+            height: auto;
+        }
+        .news-card .card-body {
+            padding: 20px;
+        }
+        .news-card h3 {
+            font-size: 24px;
+            margin-bottom: 10px;
+        }
+        .news-card p {
+            margin-bottom: 10px;
+        }
+        .news-card .card-footer {
+            background-color: #d6d6d6;
+            padding: 10px;
+            text-align: center;
+        }
+        @media (max-width: 768px) {
+            .news-card {
+                margin-bottom: 30px;
+            }
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="news-card">
+            <img src="https://via.placeholder.com/800x400" alt="">
+            <div class="card-body">
+                <h3>Manchester United vs Chelsea: Latest news</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non tellus lacus. Donec fringilla velit a velit malesuada, vel fringilla elit dignissim.</p>
+            </div>
+            <div class="card-footer">
+                <a href="#">Read More</a>
+            </div>
+        </div>
+        <div class="news-card">
+            <img src="https://via.placeholder.com/800x400" alt="">
+            <div class="card-body">
+                <h3>Arsenal vs Tottenham: Latest news</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non tellus lacus. Donec fringilla velit a velit malesuada, vel fringilla elit dignissim.</p>
+            </div>
+            <div class="card-footer">
+                <a href="#">Read More</a>
+            </div>
+        </div>
+        <div class="news-card">
+            <img src="https://via.placeholder.com/800x400" alt="">
+            <div class="card-body">
+                <h3>Liverpool vs Manchester City: Latest news</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non tellus lacus. Donec fringilla velit a velit malesuada, vel fringilla elit dignissim.</p>
+            </div>
+            <div class="card-footer">
+                <a href="#">Read More</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
