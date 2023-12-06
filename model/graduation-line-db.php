0c86de5cd7e32fdeb1fb417df2dafab10588578e
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-06 11:51:28 -0600

    Update graduation-line-db.php

--- model/graduation-line-db.php
+++ model/graduation-line-db.php
@@ -2,7 +2,7 @@
 function selectStudents() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT Graduation_Year, count(E.Enrollment_ID) as num_grad FROM `Student` S join `Enrollment` E on E.Student_ID=S.Student_ID group by Graduation_Year");
+        $stmt = $conn->prepare("SELECT PNationality, COUNT(*) AS PlayersbyNat FROM Player GROUP BY PNationality ORDER BY CountOfPlayers DESC");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
