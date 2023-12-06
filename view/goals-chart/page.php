4dd9be6cd4076b54422888e09be03b7183594644
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-06 13:00:02 -0600

    Update page.php

--- view/goals-chart/page.php
+++ view/goals-chart/page.php
@@ -37,37 +37,5 @@
   });
 </script>
 </section>
-<section>
-  <h1>Player's Nationalities</h1>
-  <div>
-  <canvas id="myChart"></canvas>
-</div>
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-<script>
-  const ctx = document.getElementById('myChart');
-
-  <?php
-  $players = selectPlayers();
-  $labels = [];
-  $data = [];
-
-  while ($player = $players->fetch_assoc()) {
-    $labels[] = "'" . $player['PNationality'] . "'";
-    $data[] = $player['PlayersbyNat'];
-  }
-  ?>
-
-  new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: [<?php echo implode(', ', $labels); ?>],
-      datasets: [{
-        label: 'Countries',
-        data: [<?php echo implode(', ', $data); ?>],
-      }],
-    },
-  });
-</script>
-</section>
 
