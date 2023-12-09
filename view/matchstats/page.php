234be519985036f7617397b62d453afbc84d9bb1
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-09 17:29:29 -0600

    Update page.php

--- view/matchstats/page.php
+++ view/matchstats/page.php
@@ -1,35 +1,96 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  </head>
-  <body>
-
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+    
 <div class="row">
     <div class="col">
-        <h1>Statistics</h1>
+        <h1>Match Statistics</h1>
     </div>
     <div class="col-auto">
-        <?php include "new-form.php"; ?>
+        <!-- Add button or other elements here if needed -->
     </div>
 </div>
-<div class="card-group">
-    <?php
-    $count = 0;
-    while ($player = $players->fetch_assoc()) {
-        if ($count % 2 == 0) {
-            echo '<div class="row">';
-        }        
-                include "player.php";
-        if ($count % 2 !== 0 || $players->num_rows % 2 !== 0) {
-            echo '</div>';
-        }
-        $count++;
-    }
-    ?>
+
+<div class="table-responsive">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Player</th>
+                <th>Data ID</th>
+                <th>Match ID</th>
+                <th>Details</th>
+                <th>Goals</th>
+                <th>Shoots On Target</th>
+                <th>Passes Completed</th>
+                <th>Chances Created</th>
+                <th>Miles Run</th>
+                <th></th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php while ($player = $players->fetch_assoc()) { ?>
+                <tr>
+                    <td><?php echo $player['PName']; ?></td>
+                    <?php $matches = selectMatchByPlayer($player['PID']);
+                    while ($match = $matches->fetch_assoc()) { ?>
+                        <td><?php echo $match['MSID']; ?></td>
+                        <td><?php echo $match['MID']; ?></td>
+                        <td><?php echo $match['MDetails']; ?></td>
+                        <td><?php echo $match['Goals_Scored']; ?></td>
+                        <td><?php echo $match['Shoots']; ?></td>
+                        <td><?php echo $match['Passes_Completed']; ?></td>
+                        <td><?php echo $match['Chances_Created']; ?></td>
+                        <td><?php echo $match['Miles_Run']; ?></td>
+                        <td>
+                            <!-- Button to trigger the collapse -->
+                            <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseMatch<?php echo $match['MSID']; ?>" aria-expanded="false" aria-controls="collapseMatch<?php echo $match['MSID']; ?>">
+                                View Match Stats
+                            </button>
+                        </td>
+                        <td>
+                            <form method="post" action="">
+                                <input type="hidden" name="Msid" value="<?php echo $match['MSID']; ?>">
+                                <input type="hidden" name="actionType" value="Delete">
+                                <button type="submit" class="btn btn-dark" onclick="return confirm('Are you sure?');">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+                                        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+                                    </svg>
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                    <!-- Collapsible row for match stats -->
+                    <tr>
+                        <td colspan="11">
+                            <div class="collapse" id="collapseMatch<?php echo $match['MSID']; ?>">
+                                <div class="card card-body">
+                                    <!-- Add your sections for each player's info here -->
+                                    <p>Goals: <?php echo $match['Goals_Scored']; ?></p>
+                                    <p>Shoots On Target: <?php echo $match['Shoots']; ?></p>
+                                    <p>Passes Completed: <?php echo $match['Passes_Completed']; ?></p>
+                                    <p>Chances Created: <?php echo $match['Chances_Created']; ?></p>
+                                    <p>Miles Run: <?php echo $match['Miles_Run']; ?></p>
+                                    <!-- Add more sections if needed -->
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                <?php } ?>
+            <?php } ?>
+        </tbody>
+    </table>
 </div>
-  </body>
+
+</body>
 </html>
