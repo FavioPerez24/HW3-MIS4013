1c29aa9df3367a699a3ed4fa9dc5c5322b95c21b
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-10 17:26:41 -0600

    Update page.php

--- view/matchstats/page.php
+++ view/matchstats/page.php
@@ -60,58 +60,60 @@
     </div>
 </div>
 
-<div class="container">
-    <div class="row row-cols-1 row-cols-md-3 g-4">
-        <?php while ($player = $players->fetch_assoc()) { ?>
-            <div class="col mb-4">
-                <div class="card">
-                    <div class="card-header">
-                        <h5 class="card-title"><?php echo $player['PName']; ?></h5>
-                    </div>
-                    <div class="card-body">
-                        <button class="btn btn-stat" type="button" data-bs-toggle="collapse" data-bs-target="#collapseStats<?php echo $player['PID']; ?>" aria-expanded="false" aria-controls="collapseStats<?php echo $player['PID']; ?>">
-                            View Stats
-                        </button>
-                    </div>
-                    <!-- Collapsible section for match stats -->
-                    <div class="collapse" id="collapseStats<?php echo $player['PID']; ?>">
-                        <div class="card card-body">
-                            <?php $matches = selectMatchByPlayer($player['PID']);
-                            while ($match = $matches->fetch_assoc()) { ?>
-                                <div class="mb-3">
-                                    <button class="btn btn-dark" type="button" data-bs-toggle="collapse" data-bs-target="#collapseMatch<?php echo $match['MSID']; ?>" aria-expanded="false" aria-controls="collapseMatch<?php echo $match['MSID']; ?>">
-                                        Match Info: <?php echo $match['MSID']; ?>
-                                    </button>
-                                    <div class="collapse" id="collapseMatch<?php echo $match['MSID']; ?>">
-                                        <div class="card card-body">
-                                            <p>Details: <?php echo $match['MDetails']; ?></p>
-                                            <p>Goals: <?php echo $match['Goals_Scored']; ?></p>
-                                            <p>Shoots On Target: <?php echo $match['Shoots']; ?></p>
-                                            <p>Passes Completed: <?php echo $match['Passes_Completed']; ?></p>
-                                            <p>Chances Created: <?php echo $match['Chances_Created']; ?></p>
-                                            <p>Miles Run: <?php echo $match['Miles_Run']; ?></p>
-                                            <?php include "edit-form1.php"; ?>
-                                            <form method="post" action="">
-                                                <input type="hidden" name="Msid" value="<?php echo $match['MSID']; ?>">
-                                                <input type="hidden" name="actionType" value="Delete">
-                                                <button type="submit" class="btn btn-dark" onclick="return confirm('Are you sure?');">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"  fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
-                                                        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
-                                                    </svg>
-                                                </button>
-                                            </form>
-                                        </div>
-                                    </div>
+<?php
+$players = selectAllPlayers();
+while ($player = $players->fetch_assoc()) {
+    $matchstats = selectMatchStatsByPlayer($player['PID']);
+?>
+    <div class="card">
+        <div class="card-header">
+            Player Name: <?php echo $player['Pname']; ?>
+        </div>
+        <div class="card-body">
+            <button class="btn btn-dark" type="button" data-bs-toggle="collapse" data-bs-target="#collapseStats<?php echo $player['PID']; ?>" aria-expanded="false" aria-controls="collapseStats<?php echo $player['PID']; ?>">
+                View Stats
+            </button>
+            <div class="collapse" id="collapseStats<?php echo $player['PID']; ?>">
+                <div class="card card-body">
+                    <?php
+                    while ($matchstat = $matchstats->fetch_assoc()) { ?>
+                        <div class="mb-3">
+                            <button class="btn btn-dark" type="button" data-bs-toggle="collapse" data-bs-target="#collapseMatchstat<?php echo $matchstat['MSID']; ?>" aria-expanded="false" aria-controls="collapseMatchstat<?php echo $matchstat['MSID']; ?>">
+                                Match Number: <?php echo $matchstat['MSID']; ?>
+                            </button>
+                            <div class="collapse" id="collapseMatchstat<?php echo $matchstat['MSID']; ?>">
+                                <div class="card card-body">
+                                    <!-- Display match details here -->
+                                   
+                                    <form method="post" action="">
+                                        <input type="hidden" name="Msid" value="<?php echo $matchstat['MSID']; ?>">
+                                        <input type="hidden" name="actionType" value="Edit">
+                                        <button type="submit" class="btn btn-dark">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                                                <!-- Pencil SVG here -->
+                                            </svg>
+                                        </button>
+                                    </form>
+                                    
+                                    <form method="post" action="">
+                                        <input type="hidden" name="Msid" value="<?php echo $matchstat['MSID']; ?>">
+                                        <input type="hidden" name="actionType" value="Delete">
+                                        <button type="submit" class="btn btn-dark" onclick="return confirm('Are you sure?');">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                                <!-- Trash SVG here -->
+                                            </svg>
+                                        </button>
+                                    </form>
+                                    
                                 </div>
-                            <?php } ?>
+                            </div>
                         </div>
-                    </div>
+                    <?php } ?>
                 </div>
             </div>
-        <?php } ?>
+        </div>
     </div>
-</div>
+<?php } ?>
 
 </body>
 </html>
