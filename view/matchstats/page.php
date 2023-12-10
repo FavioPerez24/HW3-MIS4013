8b160ed6d27786d04cec51b2902693ddf60f5fe3
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-09 19:18:20 -0600

    Update page.php

--- view/matchstats/page.php
+++ view/matchstats/page.php
@@ -10,20 +10,20 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
     <style>
         .card {
-            box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-            transition: 0.3s;
-            width: 100%; /* Updated width to ensure three cards per row */
-            border-radius: 5px;
-            margin: 10px;
-        }
+        box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+        transition: 0.3s;
+        width: 30%; /* Set the width to approximately 30% for three cards per row */
+        border-radius: 5px;
+        margin: 10px;
+    }
 
-        .card:hover {
-            box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-        }
+    .card:hover {
+        box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+    }
 
-        .container {
-            padding: 2px 16px;
-        }
+    .container {
+        padding: 2px 16px;
+    }
     </style>
     <style>
         .btn-add {
