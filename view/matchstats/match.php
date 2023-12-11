78ebfaecbfec04333a175d147a82183b1320cd4e
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-10 20:11:44 -0600

    Update match.php

--- view/matchstats/match.php
+++ view/matchstats/match.php
@@ -8,7 +8,6 @@
       <tr>
       <th>Data ID</th>
       <th>Match ID</th>
-      <th>Details</th>
       <th>Goals</th>
       <th>Shoots On Target</th>
       <th>Passes Completed</th>
@@ -22,20 +21,14 @@
       <tr>
         <td><?php echo $match['MSID']; ?></td>
         <td><?php echo $match['MID']; ?></td>
-        <td><?php echo $match['MDetails']; ?></td>
         <td><?php echo $match['Goals_Scored']; ?></td>
         <td><?php echo $match['Shoots']; ?></td>
         <td><?php echo $match['Passes_Completed']; ?></td>
         <td><?php echo $match['Chances_Created']; ?></td>
-        <td><?php echo $match['Miles_Run']; ?></td>  
-              </tr>
-
-                                        </div>
-                                        <div class="col-auto">
-                                            <?php include "edit-form1.php"; ?>
-                                        </div>
-                                        <div class="col-auto">
-                                            <form method="post" action="">
+        <td><?php echo $match['Miles_Run']; ?></td>   
+        <td><?php include "edit-form1.php"; ?></td>
+        <td>
+                                          <form method="post" action="">
                                                 <input type="hidden" name="Msid" value="<?php echo $match['MSID']; ?>">
                                                 <input type="hidden" name="actionType" value="Delete">
                                                 <button type="submit" class="btn" onclick="return confirm('Are you sure?');">
@@ -45,6 +38,7 @@
                                                     </svg>
                                                 </button>
                                             </form>
+          </td>     
                                         </div>
                                     </div>
                                 </li>
