37b5a4a95174619bc8eef24826dde3ce745c52d8
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-10 20:41:24 -0600

    Update player.php

--- view/matchstats/player.php
+++ view/matchstats/player.php
@@ -1,11 +1,18 @@
 <div class="col">
     <?php
-    $matches = selectMatchByPlayer($player['PID']); // Assuming $player is defined outside the loop
+    // Assuming $player is defined outside the loop
+    $matches = selectMatchByPlayer($player['PID']);
+    $firstIteration = true; // Flag to check the first iteration
     while ($match = $matches->fetch_assoc()) {
     ?>
         <div class="card">
             <div class="card-body">
-                <h5 class="card-title"><?php echo $player['PName']; ?></h5>
+                <?php if ($firstIteration) { ?>
+                    <h5 class="card-title"><?php echo $player['PName']; ?></h5>
+                <?php
+                    $firstIteration = false; // Set the flag to false after the first iteration
+                }
+                ?>
                 <table class="table">
                     <thead>
                         <tr>
