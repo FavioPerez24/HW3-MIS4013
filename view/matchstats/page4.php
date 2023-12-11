45fc9e733cd2bf9535e3de83e5a4ba39cd2f8793
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-10 21:18:18 -0600

    Create page4.php

---
+++ view/matchstats/page4.php
@@ -0,0 +1,17 @@
+<div class="row">
+  <div class="col">
+<h1>Match Stats by Player</h1>
+  </div>
+  <div class="col-auto">
+<?php
+include "new-form2.php";
+?>
+  </div>
+</div>
+<div class="card-group">
+<?php
+while ($player = $players->fetch_assoc()) {
+  include "player2.php";
+}
+?>
+</div>
