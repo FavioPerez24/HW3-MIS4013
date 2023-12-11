a9b40aaadce5da32a6707c9575a6b5f58318e8ac
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-10 20:27:20 -0600

    Update player.php

--- view/matchstats/player.php
+++ view/matchstats/player.php
@@ -1,17 +1,33 @@
-        <div class="col">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title"><?php echo $player['PName']; ?></h5>
-                    <p class="card-text">
-                        <ul class="list-group">
-                            <?php
-                            $matches = selectMatchByPlayer($player['PID']);
-                            while ($match = $matches->fetch_assoc()) {
-                              include "match.php";
-                           } 
-                          ?>
-                        </ul>
-                    </p>
-                </div>
+<div class="col">
+    <?php
+    $matches = selectMatchByPlayer($player['PID']); // Assuming $player is defined outside the loop
+    while ($match = $matches->fetch_assoc()) {
+    ?>
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title"><?php echo $player['PName']; ?></h5>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Data ID</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><?php echo $match['MSID']; ?></td>
+                            <!-- Add other table cells -->
+                            <td>
+                                <?php include "edit-form1.php"; ?>
+                            </td>
+                            <td>
+                                <form method="post" action="">
+                                    <!-- Form content for deletion -->
+                                </form>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
+    <?php } ?>
+</div>
