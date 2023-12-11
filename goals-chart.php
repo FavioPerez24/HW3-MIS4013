5c3088d6053681bf5f4c1b59a23e982f084b15af
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-11 13:39:47 -0600

    Update goals-chart.php

--- goals-chart.php
+++ goals-chart.php
@@ -5,7 +5,6 @@ require_once("model/goals-chart-db.php");
 $pageTitle= "OUR CHARTS";
 Include "view/header.php";
 $stats = selectStats(); 
-$players = selectPlayers();
 Include "view/goals-chart/page1.php";
 Include "view/footer.php";
 ?>
