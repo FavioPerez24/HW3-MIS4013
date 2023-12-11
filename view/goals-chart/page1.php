f933f8f6944bde0b333297874f1e7a2a651ff6d6
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-11 17:01:30 -0600

    Update page1.php

--- view/goals-chart/page1.php
+++ view/goals-chart/page1.php
@@ -5,48 +5,39 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <title>DATA PERFORMANCE</title>
+  <script src="main.js"></script>
 </head>
 <body>
-<div class="container mt-3">
-  <h1 class="text-center">OUR SCOUTING ANALYSIS</h1>
+<div class="container">
+  <button class="btn btn-primary" data-toggle="collapse" data-target="#chart1">
+    Goals by Player
+  </button>
+  <button class="btn btn-primary" data-toggle="collapse" data-target="#chart2">
+    Chances created by Player
+  </button>
+  <button class="btn btn-primary" data-toggle="collapse" data-target="#chart3">
+    Players by Nationality
+  </button>
+  <button class="btn btn-primary" data-toggle="collapse" data-target=".chart-container">
+    Show all Charts
+  </button>
+  <div class="collapse" id="chart1">
+    <canvas id="chart1-canvas"></canvas>
+  </div>
+  <div class="collapse" id="chart2">
+    <canvas id="chart2-canvas"></canvas>
+  </div>
+  <div class="collapse" id="chart3">
+    <canvas id="chart3-canvas"></canvas>
+  </div>
+  <div class="collapse" id="all-charts">
+    <div class="chart-container">
+      <canvas id="chart1-canvas"></canvas>
+      <canvas id="chart2-canvas"></canvas>
+      <canvas id="chart3-canvas"></canvas>
+    </div>
+  </div>
 </div>
-<div class="container mt-5">
-  <p class="d-inline-flex gap-1">
-    <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapse1" role="button" aria-expanded="false" aria-controls="collapse1">Goals by Player</a>
-    <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapse2" aria-expanded="false" aria-controls="collapse2">Toggle Card 2</button>
-    <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapse3" aria-expanded="false" aria-controls="collapse3">Toggle Card 3</button>
-    <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target=".multi-collapse" aria-expanded="false" aria-controls="collapse1 collapse2 collapse3">Toggle All Cards</button>
-  </p>
-
-  <div class="row mt-3">
-    <div class="col">
-      <div class="collapse multi-collapse" id="collapse1">
-        <div class="card card-body">
-          <div>
-          <?php include "chart.php"; ?>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col">
-      <div class="collapse multi-collapse" id="collapse2">
-        <div class="card card-body">
-          Some placeholder content for the second collapse component of this multi-collapse example. This panel is hidden by default but revealed when the user activates the relevant trigger.
-        </div>
-      </div>
-    </div>
-    <div class="col">
-      <div class="collapse multi-collapse" id="collapse3">
-        <div class="card card-body">
-          Some placeholder content for the third collapse component of this multi-collapse example. This panel is hidden by default but revealed when the user activates the relevant trigger.
-        </div>
-      </div>
-    </div>
-    
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-rEMycxFTAiUJwLliw8ftQAtTTJeRd1NrFIVz1FCv6ZBBdTKBhq6UohqUHEU8tOrCi" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
+  
 </body>
 </html>
