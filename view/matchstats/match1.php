76be9dcf8ffee850098fab0d4288f96d6156c10a
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-10 20:55:22 -0600

    Create match1.php

---
+++ view/matchstats/match1.php
@@ -0,0 +1,24 @@
+<table class="table">
+    <thead>
+        <tr>
+            <th>Data ID</th>
+            <th>Match ID</th>
+            <th>Goals</th>
+            <th>Shoots On Target</th>
+            <th>Passes Completed</th>
+            <th>Chances Created</th>
+            <th>Miles Run</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td><?php echo $match['MSID']; ?></td>
+            <td><?php echo $match['MID']; ?></td>
+            <td><?php echo $match['Goals_Scored']; ?></td>
+            <td><?php echo $match['Shoots']; ?></td>
+            <td><?php echo $match['Passes_Completed']; ?></td>
+            <td><?php echo $match['Chances_Created']; ?></td>
+            <td><?php echo $match['Miles_Run']; ?></td>
+        </tr>
+    </tbody>
+</table>
