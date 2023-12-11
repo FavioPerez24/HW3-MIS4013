fd68a11a29679267751e603ddf1f5a179699d060
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-10 19:27:37 -0600

    Update page3.php

--- view/matchstats/page3.php
+++ view/matchstats/page3.php
@@ -48,121 +48,71 @@
 </head>
 <body>
 
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Bootstrap demo</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <style>
-        .card {
-            box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
-            transition: 0.3s;
-            width: 80%;
-            border-radius: 5px;
-            margin: 10px;
-        }
-
-        .card:hover {
-            box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
-        }
-
-        .container {
-            padding: 2px 16px;
-        }
-    </style>
-  <style>
-    .card-img-top {
-      height: 200px; /* Set the desired height */
-      object-fit: cover; /* Crop the image to cover the specified dimensions */
-      margin: 10px 0; /* Set the desired top and bottom margins */
-    }
-    .btn-add {
-      background-color: #28a745;
-      color: #fff;
-    }
-
-    .btn-edit {
-      background-color: #007bff;
-      color: #fff;
-    }
-
-    .btn-delete {
-      background-color: #dc3545;
-      color: #fff;
-    }
-
-    .btn-teams {
-      background-color: #6c757d;
-      color: #fff;
-    }
-  </style>
-</head>
-<body>
-  <div class="container">
-    <div class="row">
-      <div class="col">
-        <h1>Match Stats by Player</h1>
-      </div>
-      <div class="col-auto">
-        <?php include "new-form.php"; ?>
-      </div>
-    </div>
-    
-<?php
-$players = selectPlayers();
-while ($player = $players->fetch_assoc()) {
-    $matches = selectMatchByPlayer($player['PID']);
-?>
-    <div class="card">
-        <div class="card-header">
-            Player Name: <?php echo $player['Pname']; ?>
-        </div>
-        <div class="card-body">
-            <button class="btn btn-dark" type="button" data-bs-toggle="collapse" data-bs-target="#collapseStats<?php echo $player['PID']; ?>" aria-expanded="false" aria-controls="collapseStats<?php echo $player['PID']; ?>">
-                View Stats
-            </button>
-            <div class="collapse" id="collapseStats<?php echo $player['PID']; ?>">
-                <div class="card card-body">
-                    <?php
-                    while ($match = $matches->fetch_assoc()) { ?>
-                        <div class="mb-3">
-                            <button class="btn btn-dark" type="button" data-bs-toggle="collapse" data-bs-target="#collapseMatch<?php echo $match['MSID']; ?>" aria-expanded="false" aria-controls="collapseMatch<?php echo $match['MSID']; ?>">
-                                Match Number: <?php echo $match['MID']; ?>
-                            </button>
-                            <div class="collapse" id="collapseMatch<?php echo $match['MSID']; ?>">
-                                <div class="card card-body">
-
-                
-                    <p>Goals: <?php echo $match['Goals_Scored']; ?></p>
-                    <p>Shoots On Target: <?php echo $match['Shoots']; ?></p>
-                    <p>Passes Completed: <?php echo $match['Passes_Completed']; ?></p>
-                    <p>Chances Created: <?php echo $match['Chances_Created']; ?></p>
-                    <p>Miles Run: <?php echo $match['Miles_Run']; ?></p>
-                    
-              <!-- Buttons in the same row -->
-              <div class="d-flex justify-content-between">
-                <?php include "edit-form1.php"; ?>
-                <form method="post" action="">
-                  <input type="hidden" name="Msid" value="<?php echo $match['MSID']; ?>">
-                  <input type="hidden" name="actionType" value="Delete">
-                  <button type="submit" class="btn btn-delete" onclick="return confirm('Are you sure?');">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
-                      <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
-                    </svg>
-                  </button>
-                </form>
-              </div>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>Match Stats by Player</h1>
             </div>
-          </div>
+            <div class="col-auto">
+                <?php include "new-form.php"; ?>
+            </div>
         </div>
-      <?php } ?>
+        
+        <?php
+        $players = selectPlayers();
+        while ($player = $players->fetch_assoc()) {
+            $matches = selectMatchByPlayer($player['PID']);
+        ?>
+            <div class="card">
+                <div class="card-header">
+                    Player Name: <?php echo $player['Pname']; ?>
+                </div>
+                <div class="card-body">
+                    <button class="btn btn-dark" type="button" data-bs-toggle="collapse" data-bs-target="#collapseStats<?php echo $player['PID']; ?>" aria-expanded="false" aria-controls="collapseStats<?php echo $player['PID']; ?>">
+                        View Stats
+                    </button>
+                    <div class="collapse" id="collapseStats<?php echo $player['PID']; ?>">
+                        <div class="card card-body">
+                            <?php
+                            while ($match = $matches->fetch_assoc()) { ?>
+                                <div class="mb-3">
+                                    <button class="btn btn-dark" type="button" data-bs-toggle="collapse" data-bs-target="#collapseMatch<?php echo $match['MSID']; ?>" aria-expanded="false" aria-controls="collapseMatch<?php echo $match['MSID']; ?>">
+                                        Match Number: <?php echo $match['MID']; ?>
+                                    </button>
+                                    <div class="collapse" id="collapseMatch<?php echo $match['MSID']; ?>">
+                                        <div class="card card-body">
+                                            <p>Goals: <?php echo $match['Goals_Scored']; ?></p>
+                                            <p>Shoots On Target: <?php echo $match['Shoots']; ?></p>
+                                            <p>Passes Completed: <?php echo $match['Passes_Completed']; ?></p>
+                                            <p>Chances Created: <?php echo $match['Chances_Created']; ?></p>
+                                            <p>Miles Run: <?php echo $match['Miles_Run']; ?></p>
+                                            
+                                            <!-- Buttons in the same row -->
+                                            <div class="d-flex justify-content-between">
+                                                <?php include "edit-form1.php"; ?>
+                                                <form method="post" action="">
+                                                    <input type="hidden" name="Msid" value="<?php echo $match['MSID']; ?>">
+                                                    <input type="hidden" name="actionType" value="Delete">
+                                                    <button type="submit" class="btn btn-delete" onclick="return confirm('Are you sure?');">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+                                                            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+                                                        </svg>
+                                                    </button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            <?php } ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
     </div>
-  </div>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     
 </body>
 </html>
