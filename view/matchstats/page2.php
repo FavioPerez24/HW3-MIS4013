0a1e248d518167f3b2f356afe15878f0b43523b6
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-10 19:54:08 -0600

    Update and rename page.css to page2.php

---
+++ view/matchstats/page2.php
@@ -0,0 +1,23 @@
+<div class="row">
+    <div class="col">
+        <h1>Match Stats by Player</h1>
+    </div>
+    <div class="col-auto">
+        <?php include "new-form.php"; ?>
+    </div>
+</div>
+<div class="card-group">
+    <?php
+    $count = 0;
+    while ($student = $students->fetch_assoc()) {
+        if ($count % 2 == 0) {
+            echo '<div class="row">';
+        }        
+                include "student.php";
+        if ($count % 2 !== 0 || $students->num_rows % 2 !== 0) {
+            echo '</div>';
+        }
+        $count++;
+    }
+    ?>
+</div>
