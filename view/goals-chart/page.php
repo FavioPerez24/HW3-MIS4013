4795d52e2ebb9ec2f9fe920c2d3b041b88053bf7
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-14 17:52:23 -0600

    Update page.php

--- view/goals-chart/page.php
+++ view/goals-chart/page.php
@@ -1,79 +1,47 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Our Scouting Analysis</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-</head>
-<body>
-
-<div class="container mt-3">
-  <h1 class="text-center">Our Scouting Analysis</h1>
-
-  <section>
-    <h1>OUR CHARTS</h1>
+<section>
+  <h1>OUR CHARTS</h1>
+</section>
     <div>
-      <div class="collapse multi-collapse" id="multiCollapseExample1">
-        <div class="card card-body">
-          <h1>Goals' Stats by Player</h1>
-          <div>
-            <div>
-          <?php include "chart.php"; ?>
-          </div>
-          </div>
-          <div class="container mt-5">
-            <div class="row">
-              <div class="col-md-8 offset-md-2">
-                <!-- Additional content or charts can be added here -->
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
-  </section>
-
-  <p class="d-inline-flex gap-1">
-    <a class="btn btn-primary" data-bs-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Toggle first element</a>
-    <!-- Add more buttons if needed -->
-  </p>
-
+<section>
+<h1>Goals' Stats by Player</h1>
+<div>
+  <div>
+  <canvas id="myChart"></canvas>
 </div>
-
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-
+</div>
+  <div class="container mt-5">
+            <div class="row">
+                <div class="col-md-8 offset-md-2">
+                    <canvas id="myChart"></canvas>
+                </div>
+            </div>
+        </div>
 <script>
-  // Ensure the DOM is fully loaded before executing Chart.js script
-  document.addEventListener('DOMContentLoaded', function () {
-    const ctx = document.getElementById('myChart').getContext('2d');
+  const ctx = document.getElementById('myChart');
 
-    <?php
-    $stats = selectStats();
-    $labels = [];
-    $data = [];
+  <?php
+  $stats = selectStats();
+  $labels = [];
+  $data = [];
 
-    while ($stat = $stats->fetch_assoc()) {
-      $labels[] = "'" . $stat['PName'] . "'";
-      $data[] = $stat['Total_goals'];
-    }
-    ?>
+  while ($stat = $stats->fetch_assoc()) {
+    $labels[] = "'" . $stat['Player_Name'] . "'";
+    $data[] = $stat['Total_goals'];
+  }
+  ?>
 
-    new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: [<?php echo implode(', ', $labels); ?>],
-        datasets: [{
-          label: 'Total Goals per Player',
-          data: [<?php echo implode(', ', $data); ?>],
-        }],
-      },
-    });
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: [<?php echo implode(', ', $labels); ?>],
+      datasets: [{
+        label: 'Total Goals per Player',
+        data: [<?php echo implode(', ', $data); ?>],
+      }],
+    },
   });
 </script>
+</section>
 
-</body>
-</html>
