fbfdeafa96b98ed1913973cf0fad572ccd39ef9a
FavioPerez24 <123128136+FavioPerez24@users.noreply.github.com>
2023-12-13 21:56:22 -0600

    Update page.php

--- news/page.php
+++ news/page.php
@@ -6,7 +6,7 @@
   <title>Top Stories</title>
   <link rel="stylesheet" href="styles.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-  <?php Include "view/header.php"; ?>
+  <?php include "view/header.php"; ?>
   <style>
         .all-browsers {
             background-color: black;
